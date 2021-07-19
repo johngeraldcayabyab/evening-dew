8553c967d8ea84da013d85aae2f36067571dedc4
Dlareg <johngeraldcayabyab@gmail.com>
2021-07-19 23:46:50 +0800

    Created models and seeder for unit of measurements categories

--- database/migrations/2021_07_19_144140_create_unit_of_measurements_categories.php
+++ database/migrations/2021_07_19_144140_create_unit_of_measurements_categories.php
@@ -10,9 +10,7 @@ class CreateUnitOfMeasurementsCategories extends Migration
     {
         Schema::create('unit_of_measurements_categories', function (Blueprint $table) {
             $table->id();
-            $table->enum('type', ['reference', 'bigger', 'smaller']);
-            $table->decimal('rounding_precision', 19, 4)->default(0.01000);
-            $table->decimal('ratio', 19, 4)->default(1.000000);
+            $table->string('name');
             $table->softDeletes();
             $table->timestamps();
         });
--- database/seeders/UnitOfMeasurementCategorySeeder.php
+++ database/seeders/UnitOfMeasurementCategorySeeder.php
@@ -2,12 +2,30 @@
 
 namespace Database\Seeders;
 
+use App\Models\UnitOfMeasurementCategory;
+use App\Modules\UnitOfMeasurementsCategories\Services\UnitOfMeasurementCategoryStore;
 use Illuminate\Database\Seeder;
 
 class UnitOfMeasurementCategorySeeder extends Seeder
 {
+    private $unitOfMeasurementCategoryStore;
+
+    public function __construct(UnitOfMeasurementCategory $unitOfMeasurementCategory)
+    {
+        $this->unitOfMeasurementCategoryStore = new UnitOfMeasurementCategoryStore($unitOfMeasurementCategory);
+    }
+
     public function run()
     {
-        
+        $data = [
+            ['name' => 'Units'],
+            ['name' => 'Units'],
+            ['name' => 'Units'],
+            ['name' => 'Units'],
+        ];
+
+        foreach ($data as $datum) {
+            $this->unitOfMeasurementCategoryStore->store($datum);
+        }
     }
 }
