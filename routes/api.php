65d9861d49063b7c98af885cd0d0b16430079254
Dlareg <johngeraldcayabyab@gmail.com>
2021-07-21 20:53:36 +0800

    Fixing unit of measurement category controllers

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResources([
-    'units_of_measure_categories' => UnitOfMeasureCategoryController::class
-]);
+Route::apiResource('units_of_measure_categories',  UnitOfMeasureCategoryController::class);
+
