75eac5da6f05655c126a1a2f33749bb24ada9f4a
Gerald <johngeraldcayabyab@gmail.com>
2021-08-05 17:07:23 +0800

    Test create form

---
+++ resources/js/UnitOfMeasureCategory/UnitOfMeasureCategoryForm.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Button, Form, Input} from "antd";
+
+const UnitOfMeasureCategoryForm = () => {
+
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+        <Form
+            name="basic"
+            labelCol={{span: 8}}
+            wrapperCol={{span: 16}}
+            initialValues={{remember: true}}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+        >
+            <Form.Item
+                label="Name"
+                name="name"
+                rules={[{required: true, message: 'Please input name!'}]}
+            >
+                <Input/>
+            </Form.Item>
+
+
+            <Form.Item wrapperCol={{offset: 8, span: 16}}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+};
+
+export default UnitOfMeasureCategoryForm;
