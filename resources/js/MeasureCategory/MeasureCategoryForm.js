c5424c93495341b7647acc9395f3efdec3088fa2
Dlareg <johngeraldcayabyab@gmail.com>
2021-08-10 18:25:36 +0800

    moved form handling inside hooks

--- resources/js/MeasureCategory/MeasureCategoryForm.js
+++ resources/js/MeasureCategory/MeasureCategoryForm.js
@@ -1,58 +1,12 @@
-import React, {useEffect, useState} from 'react';
-import {Button, Form, Input, message} from "antd";
-import {useHistory, useParams} from "react-router-dom";
+import React from 'react';
+import {Button, Form, Input} from "antd";
+import {useParams} from "react-router-dom";
+import useInitialValues from "../Hooks/useInitialValues";
 
 const MeasureCategoryForm = () => {
     let {id} = useParams();
     const [form] = Form.useForm();
-    const history = useHistory();
-    const [errors, setErrors] = useState({});
-
-    useEffect(async () => {
-        if (id) {
-            let responseData = await fetch(`/api/measures_categories/${id}`)
-                .then(response => response.json())
-                .then(data => (data));
-            form.setFieldsValue(responseData);
-        }
-    }, []);
-
-    const onFinish = async (values) => {
-        let url = `/api/measures_categories/`;
-        let method = 'POST';
-        if (id) {
-            url += id;
-            method = 'PUT';
-        }
-        await fetch(url, {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            method: method,
-            body: JSON.stringify(values)
-        }).then(response => {
-            if (response.ok) {
-                return response;
-            }
-            throw response;
-        }).then(result => {
-            let headerLocation = result.headers.get('Location');
-            if (headerLocation) {
-                history.push(headerLocation);
-            }
-        }).catch(error => {
-            let status = error.status;
-            error.json().then((body) => {
-                if (status === 422) {
-                    message.warning(body.message);
-                } else if (status === 500) {
-                    message.error(body.message);
-                }
-                setErrors(body.errors);
-            });
-        });
-    };
+    const [errors, onFinish] = useInitialValues(id, form);
 
     return (
         <Form
