065bb6b4693057433bf3d470eed4a25b70774804
Gerald <johngeraldcayabyab@gmail.com>
2021-08-11 21:23:19 +0800

    Learning redis, redis is good for caching menus

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redis;
 
-Route::view('/{path?}', 'app')->where('path', '.*')->name('react');
+/**
+ * Test mode
+ */
+Route::get('/', function(){
+
+    $user3Stats = [
+        'favorites' => 420,
+        'watchLaters' => 420,
+        'completions' => 420,
+    ];
+
+    Redis::hmset('user.3.stats', $user3Stats);
+
+//    $redis = new Illuminate\Redis\
+    return [
+        Redis::hgetall('user.1.stats'),
+        Redis::hgetall('user.2.stats')
+    ];
+//    return Redis::hgetall('user.2.stats');
+});
+
+/**
+ * React
+ */
+//Route::view('/{path?}', 'app')->where('path', '.*')->name('react');
