f9b8a04d126236f48d09a6f845874f2987101e53
Gerald <johngeraldcayabyab@gmail.com>
2021-08-14 23:57:13 +0800

    On update, data refetches

--- resources/js/Menu/MenuForm.js
+++ resources/js/Menu/MenuForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Form, Input} from "antd";
+import {Button, Form, Input, Skeleton} from "antd";
 import {useParams} from "react-router-dom";
 import useFormState from "../Hooks/useFormState";
 import manifest from "./__manifest__.json";
@@ -10,39 +10,41 @@ const MenuForm = () => {
     const [formState, formActions] = useFormState(id, form, manifest);
 
     return (
-        <Form
-            form={form}
-            name="basic"
-            labelCol={{span: 8}}
-            wrapperCol={{span: 16}}
-            onFinish={formActions.onFinish}
-        >
-            <Form.Item
-                label="Label"
-                name="label"
-                validateStatus={formState.errors.label ? 'error' : false}
-                help={formState.errors.label ? formState.errors.label : false}
-                rules={[{required: true, message: 'Please input measure name'}]}
+        <Skeleton loading={formState.loading}>
+            <Form
+                form={form}
+                name="basic"
+                labelCol={{span: 8}}
+                wrapperCol={{span: 16}}
+                onFinish={formActions.onFinish}
             >
-                <Input/>
-            </Form.Item>
+                <Form.Item
+                    label="Label"
+                    name="label"
+                    validateStatus={formState.errors.label ? 'error' : false}
+                    help={formState.errors.label ? formState.errors.label : false}
+                    rules={[{required: true, message: 'Please input measure name'}]}
+                >
+                    <Input/>
+                </Form.Item>
 
-            <Form.Item
-                label="Url"
-                name="url"
-                validateStatus={formState.errors.url ? 'error' : false}
-                help={formState.errors.url ? formState.errors.url : false}
-                rules={[{required: true, message: 'Please input measure name'}]}
-            >
-                <Input/>
-            </Form.Item>
+                <Form.Item
+                    label="Url"
+                    name="url"
+                    validateStatus={formState.errors.url ? 'error' : false}
+                    help={formState.errors.url ? formState.errors.url : false}
+                    rules={[{required: true, message: 'Please input measure name'}]}
+                >
+                    <Input/>
+                </Form.Item>
 
-            <Form.Item wrapperCol={{offset: 8, span: 16}}>
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form>
+                <Form.Item wrapperCol={{offset: 8, span: 16}}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </Skeleton>
     );
 };
 
