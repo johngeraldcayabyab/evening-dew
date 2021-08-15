234935ba16355b171f51d33d972917738b82ad1e
Dlareg <johngeraldcayabyab@gmail.com>
2021-08-15 13:18:52 +0800

    added a cache flush in observer if menu is updated. there must be a better way on doing this

--- app/Observers/MenuObserver.php
+++ app/Observers/MenuObserver.php
@@ -4,22 +4,25 @@ namespace App\Observers;
 
 use App\Events\MenuEvent;
 use App\Models\Menu;
-use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Cache;
 
 class MenuObserver
 {
     public function created(Menu $model)
     {
+        Cache::forget('menus.all');
         MenuEvent::dispatch($model, 'created');
     }
 
     public function updated(Menu $model)
     {
+        Cache::forget('menus.all');
         MenuEvent::dispatch($model, 'updated');
     }
 
     public function deleted(Menu $model)
     {
+        Cache::forget('menus.all');
         MenuEvent::dispatch($model, 'deleted');
     }
 }
