ac3acf498d7f4f3ca6419810c8cafb59d01bfceb
Dlareg <johngeraldcayabyab@gmail.com>
2021-11-02 23:45:21 +0800

    breadcrumbs scenarios fixed

--- resources/js/components/DynamicBreadcrumbs.js
+++ resources/js/components/DynamicBreadcrumbs.js
@@ -6,35 +6,24 @@ import {replaceUnderscoreWithSpace, titleCase, uuidv4} from "../Helpers/string";
 
 const DynamicBreadcrumbs = () => {
     const location = useLocation();
-    const [breadcrumbs, setBreadcrumbs] = useState([])
-
-
-    const isCreatePagePath = (splitPathName) => {
-        return splitPathName.length === 3 && splitPathName[2] === 'create';
-    }
-
-    const isEditPagePath = (splitPathName) => {
-        return splitPathName.length === 3 && splitPathName[2] !== 'create';
-    }
-
-    const isMainPath = (splitPathName) => {
-        return splitPathName.length === 2;
-    }
-
+    const [breadcrumbs, setBreadcrumbs] = useState([]);
 
     useEffect(async () => {
         let pathname = location.pathname;
         let newSlug = {};
         let splitPathName = pathname.split('/');
-        let pathRefresh = false;
-        if (isEditPagePath(splitPathName)) {
-            splitPathName = '/api' + splitPathName.join('/') + '/slug';
-            let responseData = await fetch(splitPathName)
+
+        let isCreatePagePath = splitPathName.length === 3 && splitPathName[2] === 'create';
+        let isEditPagePath = splitPathName.length === 3 && splitPathName[2] !== 'create';
+        let isMainPath = splitPathName.length === 2;
+
+        if (isEditPagePath) {
+            let responseData = await fetch(`/api${splitPathName.join('/')}/slug`)
                 .then(response => response.json())
                 .then(data => (data));
             responseData.link = pathname;
             newSlug = responseData;
-        } else if (isCreatePagePath(splitPathName)) {
+        } else if (isCreatePagePath) {
             newSlug = {
                 key: uuidv4(),
                 slug: 'New',
@@ -46,21 +35,46 @@ const DynamicBreadcrumbs = () => {
                 slug: titleCase(replaceUnderscoreWithSpace(splitPathName[1])),
                 link: pathname
             };
-            pathRefresh = true;
         }
 
         setBreadcrumbs(previousState => {
             let newState = [...previousState];
-            if (pathRefresh) {
+            if (isMainPath) {
                 newState = [];
             }
 
             /**
              *Cuts path back if path exists;
              */
-            let isNewPathIndexExists = previousState.findIndex(state => state.link === newSlug.link);
-            if (Math.max(0, isNewPathIndexExists)) {
-                newState = newState.slice(0, isNewPathIndexExists);
+            let isNewPathExists = previousState.findIndex(state => state.link === newSlug.link);
+            if (Math.max(0, isNewPathExists)) {
+                newState = newState.slice(0, isNewPathExists);
+            }
+
+            /**
+             * Adds a parent breadcrumb if path is lonely
+             */
+            if (newState.length === 0 && !isMainPath) {
+                let newPathname = pathname.split('/');
+                newPathname.pop();
+                newPathname = newPathname.join('/');
+                newState.push({
+                    key: uuidv4(),
+                    slug: titleCase(replaceUnderscoreWithSpace(splitPathName[1])),
+                    link: newPathname
+                });
+            }
+
+            /**
+             * Remove "Create Pages" if page has been redirected to
+             * the created page
+             */
+            if (isEditPagePath) {
+                newState = newState.filter((state) => {
+                    if (state.slug !== 'New') {
+                        return state;
+                    }
+                });
             }
 
             newState = newState.map((state) => {
@@ -68,6 +82,7 @@ const DynamicBreadcrumbs = () => {
                 return state;
             });
             newSlug.isLink = false;
+
             newState.push(newSlug);
             return newState;
         });
