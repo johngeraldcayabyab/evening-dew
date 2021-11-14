74a7ddc04084ff5ea2e99f7479d66403a3a6705b
Dlareg <johngeraldcayabyab@gmail.com>
2021-11-14 13:09:12 +0800

    Added basic automatic event register

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,8 +6,9 @@ use App\Modules\Measurement\Models\Measurement;
 use App\Modules\Measurement\Observers\MeasurementObserver;
 use App\Modules\MeasurementCategory\Models\MeasurementCategory;
 use App\Modules\MeasurementCategory\Observers\MeasurementCategoryObserver;
-use App\Modules\Menu\Models\Menu;
-use App\Modules\Menu\Observers\MenuObserver;
+
+//use App\Modules\Menu\Models\Menu;
+//use App\Modules\Menu\Observers\MenuObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -22,8 +23,24 @@ class EventServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        MeasurementCategory::observe(MeasurementCategoryObserver::class);
-        Measurement::observe(MeasurementObserver::class);
-        Menu::observe(MenuObserver::class);
+        $this->observe();
+    }
+
+    private function observe()
+    {
+        $modules = [
+            'MeasurementCategory',
+            'Measurement',
+            'Menu'
+        ];
+
+        foreach ($modules as $module) {
+            $modulePath = "\\App\Modules\\$module\\";
+            $modelNamespace = $modulePath . "Models\\" . $module;
+            $observerNamespace = $modulePath . "Observers\\" . $module . "Observer";
+            $observerNamespace = new $observerNamespace;
+            $observerNamespace = get_class($observerNamespace);
+            $modelNamespace::observe($observerNamespace);
+        }
     }
 }
