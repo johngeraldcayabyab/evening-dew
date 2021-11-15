7a62d295ae32b597db7722f0bc5098fdd87c046d
Gerald <johngeraldcayabyab@gmail.com>
2021-11-15 21:29:03 +0800

    added name to routes and used named routes for the location of the created resource

--- app/Modules/Measurement/Routes/MeasurementRoute.php
+++ app/Modules/Measurement/Routes/MeasurementRoute.php
@@ -9,11 +9,11 @@ class MeasurementRoute
 {
     public static function initialize()
     {
-        Route::get('measurements/{measurement}/slug', [MeasurementController::class, 'slug']);
-        Route::get('measurements/{measurement}', [MeasurementController::class, 'show']);
-        Route::put('measurements/{measurement}', [MeasurementController::class, 'update']);
-        Route::delete('measurements/{measurement}', [MeasurementController::class, 'destroy']);
-        Route::post('measurements', [MeasurementController::class, 'store']);
-        Route::get('measurements', [MeasurementController::class, 'index']);
+        Route::get('measurements/{measurement}/slug', [MeasurementController::class, 'slug'])->name('measurements.slug');
+        Route::get('measurements/{measurement}', [MeasurementController::class, 'show'])->name('measurements.show');
+        Route::put('measurements/{measurement}', [MeasurementController::class, 'update'])->name('measurements.update');
+        Route::delete('measurements/{measurement}', [MeasurementController::class, 'destroy'])->name('measurements.destroy');
+        Route::post('measurements', [MeasurementController::class, 'store'])->name('measurements.store');
+        Route::get('measurements', [MeasurementController::class, 'index'])->name('measurements.index');
     }
 }
--- app/Modules/Menu/Routes/MenuRoute.php
+++ app/Modules/Menu/Routes/MenuRoute.php
@@ -9,11 +9,11 @@ class MenuRoute
 {
     public static function initialize()
     {
-        Route::get('menus/{menu}/slug', [MenuController::class, 'slug']);
-        Route::get('menus/{menu}', [MenuController::class, 'show']);
-        Route::put('menus/{menu}', [MenuController::class, 'update']);
-        Route::delete('menus/{menu}', [MenuController::class, 'destroy']);
-        Route::post('menus', [MenuController::class, 'store']);
-        Route::get('menus', [MenuController::class, 'index']);
+        Route::get('menus/{menu}/slug', [MenuController::class, 'slug'])->name('menus.slug');
+        Route::get('menus/{menu}', [MenuController::class, 'show'])->name('menus.show');
+        Route::put('menus/{menu}', [MenuController::class, 'update'])->name('menus.update');
+        Route::delete('menus/{menu}', [MenuController::class, 'destroy'])->name('menus.destroy');
+        Route::post('menus', [MenuController::class, 'store'])->name('menus.store');
+        Route::get('menus', [MenuController::class, 'index'])->name('menus.index');
     }
 }
