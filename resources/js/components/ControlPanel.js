b1041edbcc0f3d1a6b14266a31d04fa28517d120
Gerald <johngeraldcayabyab@gmail.com>
2021-11-19 18:03:11 +0800

    deposit

--- resources/js/components/ControlPanel.js
+++ resources/js/components/ControlPanel.js
@@ -6,18 +6,30 @@ const ControlPanel = (props) => {
     return (
         <div style={{padding: '5px 16px', borderBottom: '1px solid #cccccc', background: '#fff'}}>
             <Row align={'middle'} style={{marginTop: '5px', marginBottom: '5px'}}>
-                <Col span={12}>
+                <Col span={6}>
                     <CustomBreadcrumb/>
                 </Col>
-                <Col span={12}>
+                <Col span={6}>
+                    {props.topRight}
+                </Col>
+                <Col span={6}>
+                    {props.topRight}
+                </Col>
+                <Col span={6}>
                     {props.topRight}
                 </Col>
             </Row>
             <Row align={'middle'} style={{marginTop: '5px', marginBottom: '5px'}}>
-                <Col span={12}>
+                <Col span={6}>
                     {props.bottomLeft}
                 </Col>
-                <Col span={12}>
+                <Col span={6}>
+                    {props.bottomRight}
+                </Col>
+                <Col span={6}>
+                    {props.bottomRight}
+                </Col>
+                <Col span={6}>
                     {props.bottomRight}
                 </Col>
             </Row>
