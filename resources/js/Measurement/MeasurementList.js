fff24a21407224c7505a256dbd5b2219d011e819
Gerald <johngeraldcayabyab@gmail.com>
2021-11-25 21:53:31 +0800

    creating an action buttons for all selectable rows

--- resources/js/Measurement/MeasurementList.js
+++ resources/js/Measurement/MeasurementList.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Button, Layout, Popconfirm, Space, Table} from "antd";
 import useListState from "../Hooks/useListState";
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 import manifest from "./__manifest__.json";
 import {DeleteOutlined, EyeOutlined} from "@ant-design/icons";
 import TableCreateButton from "../components/ActionButtons/TableCreateButton";
 import ControlPanel from "../components/ControlPanel";
+import CustomTable from "../components/CustomTable";
 
 const MeasurementList = () => {
+
+
     const [tableState, tableActions] = useListState(manifest, [
         {
             title: 'Name',
@@ -29,45 +32,37 @@ const MeasurementList = () => {
             dataIndex: 'rounding_precision',
             key: 'rounding_precision',
         },
-        {
-            title: 'Action',
-            dataIndex: '',
-            key: 'x',
-            render: (data) => {
-                return (
-                    <Space>
-                        <Button size={"small"} type="primary">
-                            <Link to={`/${manifest.moduleName}/${data.id}`}><EyeOutlined/></Link>
-                        </Button>
-                        <Popconfirm title={`Are you sure delete this ${manifest.moduleName}?`} okText="Yes"
-                                    cancelText="No" onConfirm={() => {
-                            tableActions.handleDelete(data.id);
-                        }}>
-                            <Button size={"small"} type="primary" danger>
-                                <DeleteOutlined/>
-                            </Button>
-                        </Popconfirm>
-                    </Space>
-                );
-            }
-        },
+        // {
+        //     title: 'Action',
+        //     dataIndex: '',
+        //     key: 'x',
+        //     render: (data) => {
+        //         return (
+        //             <Space>
+        //                 <Button size={"small"} type="primary">
+        //                     <Link to={`/${manifest.moduleName}/${data.id}`}><EyeOutlined/></Link>
+        //                 </Button>
+        //                 <Popconfirm title={`Are you sure delete this ${manifest.moduleName}?`} okText="Yes"
+        //                             cancelText="No" onConfirm={() => {
+        //                     tableActions.handleDelete(data.id);
+        //                 }}>
+        //                     <Button size={"small"} type="primary" danger>
+        //                         <DeleteOutlined/>
+        //                     </Button>
+        //                 </Popconfirm>
+        //             </Space>
+        //         );
+        //     }
+        // },
     ]);
-
-    // const columns = ;
 
     return (
         <React.Fragment>
             <ControlPanel
                 bottomColOneLeft={<TableCreateButton manifest={manifest}/>}
+                bottomColOneRight={}
             />
-            <Table
-                loading={tableState.loading}
-                size={'small'}
-                dataSource={tableState.dataSource}
-                columns={tableState.columns}
-                rowKey={'id'}
-                onRow={tableState.onRow}
-            />
+            <CustomTable {...tableState} {...tableActions} manifest={manifest}/>
         </React.Fragment>
     )
 };
