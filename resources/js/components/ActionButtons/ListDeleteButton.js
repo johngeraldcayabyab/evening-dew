5e45638f3f150eba8e01fb8adee49db452e78e2b
Dlareg <johngeraldcayabyab@gmail.com>
2021-11-27 22:37:14 +0800

    passed list state down to button

--- resources/js/components/ActionButtons/ListDeleteButton.js
+++ resources/js/components/ActionButtons/ListDeleteButton.js
@@ -1,13 +1,15 @@
 import {Menu, Popconfirm} from "antd";
 import React from "react";
-import {uuidv4} from "../../Helpers/string";
 
 const ListDeleteButton = (props) => {
     return (
         <Menu.Item key={'deleter'}>
-            <Popconfirm title={`Are you sure you want to delete the selected items?`} okText="Yes"
-                        cancelText="No" onConfirm={() => {
-                // tableActions.handleDelete(data.id);
+            <Popconfirm
+                title={`Are you sure you want to delete the selected items?`}
+                okText="Yes"
+                cancelText="No" onConfirm={() => {
+                console.log(props.handleMassDelete());
+                // props.tableActions.handleMassDelete();
             }}>
                 Delete
             </Popconfirm>
