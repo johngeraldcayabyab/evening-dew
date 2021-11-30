4da7ec5b0997c28a80ce037f96575a9551f4eecf
Dlareg <johngeraldcayabyab@gmail.com>
2021-11-30 23:18:12 +0800

    Debugging laravel event not broadcasting

--- app/Modules/Measurement/Events/MeasurementEvent.php
+++ app/Modules/Measurement/Events/MeasurementEvent.php
@@ -5,10 +5,11 @@ namespace App\Modules\Measurement\Events;
 use App\Modules\Measurement\Models\Measurement;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MeasurementCrudEvent
+class MeasurementEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
@@ -17,6 +18,7 @@ class MeasurementCrudEvent
 
     public function __construct(Measurement $model, $method)
     {
+        info('is broadcast');
         $this->model = $model;
         $this->method = $method;
     }
