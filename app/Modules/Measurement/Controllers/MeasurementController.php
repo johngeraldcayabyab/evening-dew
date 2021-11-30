735e38a4862e65158849651a67a1c77fbb53bcd9
Gerald <johngeraldcayabyab@gmail.com>
2021-11-30 23:18:12 +0800

    Debugging laravel event not broadcasting

--- app/Modules/Measurement/Controllers/MeasurementController.php
+++ app/Modules/Measurement/Controllers/MeasurementController.php
@@ -24,6 +24,7 @@ class MeasurementController
 
     public function store(MeasurementStoreRequest $request): JsonResponse
     {
+        info('egg bert');
         $headers = location_header(route('menus.show', Measurement::create($request->validated())));
         return response()->json([], STATUS_CREATE, $headers);
     }
