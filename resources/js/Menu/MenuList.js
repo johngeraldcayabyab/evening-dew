3cc6ef9d45b3d9227df6e88b9c0123191919c6cb
Gerald <johngeraldcayabyab@gmail.com>
2021-12-07 22:42:15 +0800

    unified list formation

--- resources/js/Menu/MenuList.js
+++ resources/js/Menu/MenuList.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import {Button, Card, Layout, Popconfirm, Space, Table} from "antd";
 import useListState from "../Hooks/useListState";
-import {Link} from "react-router-dom";
 import manifest from "./__manifest__.json";
-import {EyeOutlined, DeleteOutlined} from '@ant-design/icons';
 import TableCreateButton from "../components/ActionButtons/TableCreateButton";
 import ControlPanel from "../components/ControlPanel";
+import ActionsDropdownButton from "../components/ActionButtons/ActionsDropdownButton";
+import CustomTable from "../components/CustomTable";
 
 const MenuList = () => {
     const [tableState, tableActions] = useListState(manifest, [
@@ -19,44 +18,15 @@ const MenuList = () => {
             dataIndex: 'url',
             key: 'url',
         },
-        {
-            title: 'Action',
-            dataIndex: '',
-            key: 'x',
-            render: (data) => {
-                return (
-                    <Space>
-                        <Button size={"small"} type="primary">
-                            <Link to={`/${manifest.moduleName}/${data.id}`}><EyeOutlined/></Link>
-                        </Button>
-                        <Popconfirm title={`Are you sure delete this ${manifest.moduleName}?`} okText="Yes"
-                                    cancelText="No" onConfirm={() => {
-                            tableActions.handleDelete(data.id);
-                        }}>
-                            <Button size={"small"} type="primary" danger>
-                                <DeleteOutlined/>
-                            </Button>
-                        </Popconfirm>
-                    </Space>
-
-                );
-            }
-        },
     ]);
 
     return (
         <React.Fragment>
             <ControlPanel
                 bottomColOneLeft={<TableCreateButton manifest={manifest}/>}
+                bottomColOneRight={<ActionsDropdownButton {...tableState} {...tableActions} manifest={manifest}/>}
             />
-            <Table
-                loading={tableState.loading}
-                size={'small'}
-                dataSource={tableState.dataSource}
-                columns={tableState.columns}
-                rowKey={'id'}
-                onRow={tableState.onRow}
-            />
+            <CustomTable {...tableState} {...tableActions} manifest={manifest}/>
         </React.Fragment>
     )
 };
