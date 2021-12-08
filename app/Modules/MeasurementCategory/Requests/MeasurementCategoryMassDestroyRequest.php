164b7af0d0105ec7dee39335954cde24b8384207
Dlareg <johngeraldcayabyab@gmail.com>
2021-12-08 22:43:09 +0800

    added rules for mass destroy in other modules

--- app/Modules/MeasurementCategory/Requests/MeasurementCategoryMassDestroyRequest.php
+++ app/Modules/MeasurementCategory/Requests/MeasurementCategoryMassDestroyRequest.php
@@ -2,7 +2,14 @@
 
 namespace App\Modules\MeasurementCategory\Requests;
 
-class MeasurementCategoryMassDestroyRequest
-{
+use Illuminate\Foundation\Http\FormRequest;
 
+class MeasurementCategoryMassDestroyRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'ids.*' => 'exists:measurement_categories,id'
+        ];
+    }
 }
--- app/Modules/Menu/Requests/MenuMassDestroyRequest.php
+++ app/Modules/Menu/Requests/MenuMassDestroyRequest.php
@@ -2,7 +2,14 @@
 
 namespace App\Modules\Menu\Requests;
 
-class MenuMassDestroyRequest
-{
+use Illuminate\Foundation\Http\FormRequest;
 
+class MenuMassDestroyRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'ids.*' => 'exists:menus,id'
+        ];
+    }
 }
