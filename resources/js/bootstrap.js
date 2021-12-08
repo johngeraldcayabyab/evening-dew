ecc8720610075b1090870be19545998daa4b451e
Gerald <johngeraldcayabyab@gmail.com>
2021-12-08 22:11:41 +0800

    fix pusher env getter

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,13 +4,9 @@ import Echo from 'laravel-echo';
 
 window.Pusher = require('pusher-js');
 
-console.log(process.env.MIX_PUSHER_APP_KEY);
-
-console.log('egg');
-
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: "123",
-    cluster: "123",
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     forceTLS: true
 });
