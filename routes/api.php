f8412ac733ce2d63b9dc9ee408a891603eaa6474
Dlareg <johngeraldcayabyab@gmail.com>
2021-12-11 09:06:32 +0800

    added request token in api, will try fortify for auth

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
+
 Route::group(['middleware' => ['api', 'cors']], function () {
     MeasurementCategoryRoute::initialize();
     MeasurementRoute::initialize();
