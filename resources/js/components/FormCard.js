5f07c695f0220b1e24743c5e21547f8e86bb9779
johngeraldcayabyab@gmail.com <johngeraldcayabyab@gmail.com>
2021-12-24 13:52:26 +0800

    Added background in card

--- resources/js/components/FormCard.js
+++ resources/js/components/FormCard.js
@@ -7,6 +7,7 @@ const FormCard = (props) => {
             maxWidth: '1140px',
             minHeight: '330px',
             margin: '12px auto 0 auto',
+            boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
         }}>
             {props.loading ? null : props.children}
         </Card>
