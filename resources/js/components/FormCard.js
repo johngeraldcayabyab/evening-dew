8fce68b29df341d385e6973111bd2029757ef296
johngeraldcayabyab@gmail.com <johngeraldcayabyab@gmail.com>
2021-12-25 13:19:34 +0800

    added cool background to form card, and fixed pointer when exiting list

--- resources/js/components/FormCard.js
+++ resources/js/components/FormCard.js
@@ -2,15 +2,24 @@ import {Card} from "antd";
 
 const FormCard = (props) => {
     return (
-        <Card style={{
-            minWidth: '650px',
-            maxWidth: '1140px',
-            minHeight: '330px',
-            margin: '12px auto 0 auto',
-            boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+        <div style={{
+            backgroundImage: `linear-gradient(45deg, transparent 20%, #cccccc 25%, transparent 25%),
+            linear-gradient(-45deg, transparent 20%, #cccccc 25%, transparent 25%),
+            linear-gradient(-45deg, transparent 75%, #cccccc 80%, transparent 0),
+            radial-gradient(#cccccc 2px, transparent 0)`,
+            backgroundSize: '15px 15px, 15px 15px',
+            padding: '15px 0'
         }}>
-            {props.loading ? null : props.children}
-        </Card>
+            <Card style={{
+                minWidth: '650px',
+                maxWidth: '1140px',
+                minHeight: '330px',
+                margin: '0 auto',
+                boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+            }}>
+                {props.loading ? null : props.children}
+            </Card>
+        </div>
     )
 }
 
