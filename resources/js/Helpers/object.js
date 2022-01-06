2bd0509834d785292d8c92b57963cf4e186af822
Dlareg <johngeraldcayabyab@gmail.com>
2022-01-06 21:20:36 +0800

    object  filter can now send correct payload

---
+++ resources/js/Helpers/object.js
@@ -0,0 +1,8 @@
+export const cleanObject = (obj) => {
+    for (let propName in obj) {
+        if (obj[propName] === null || obj[propName] === undefined) {
+            delete obj[propName];
+        }
+    }
+    return obj;
+}
