3fe1cd4c989eee459413d44b2b33b84f8877a573
johngeraldcayabyab@gmail.com <johngeraldcayabyab@gmail.com>
2022-01-08 13:11:27 +0800

    improved background mesh of form card

--- resources/js/components/FormCard.js
+++ resources/js/components/FormCard.js
@@ -3,12 +3,19 @@ import {Card} from "antd";
 const FormCard = (props) => {
     return (
         <div style={{
-            backgroundImage: `linear-gradient(45deg, transparent 20%, #cccccc 25%, transparent 25%),
-            linear-gradient(-45deg, transparent 20%, #cccccc 25%, transparent 25%),
-            linear-gradient(-45deg, transparent 75%, #cccccc 80%, transparent 0),
-            radial-gradient(#cccccc 2px, transparent 0)`,
-            backgroundSize: '15px 15px, 15px 15px',
-            padding: '15px 0'
+            // backgroundImage: `linear-gradient(45deg, transparent 20%, #cccccc 25%, transparent 25%),
+            // linear-gradient(-45deg, transparent 20%, #cccccc 25%, transparent 25%),
+            // linear-gradient(-45deg, transparent 75%, #cccccc 80%, transparent 0),
+            // radial-gradient(#cccccc 2px, transparent 0)`,
+            // backgroundSize: '15px 15px, 15px 15px',
+
+            backgroundImage: 'linear-gradient(rgba(204, 204, 204, 0.2) 0.1em, transparent 0.2em), linear-gradient(90deg, rgba(204, 204, 204, 0.2) 0.1em, transparent 0.1em)',
+            backgroundSize: '.4em 0.4em',
+
+
+
+            padding: '15px 0',
+            borderBottom: '1px solid #cccccc'
         }}>
             <Card style={{
                 minWidth: '650px',
