c668d34c8d191eb337dad2f79c32d0479ed7c835
Dlareg <johngeraldcayabyab@gmail.com>
2022-01-12 23:59:12 +0800

    adding authentication

---
+++ app/Http/Controllers/AuthenticateController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AuthenticateController
+{
+    public function login()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+}
--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,7 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
 class LoginController
 {
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = User::where('email', $request->email)->first();
+            return $user->createToken($request->device_name)->plainTextToken;
+        }
+
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
 
 }
