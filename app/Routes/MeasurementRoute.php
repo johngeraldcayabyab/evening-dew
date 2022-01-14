f4e8436137bcc8138c228fd93c63c0eaba9128fc
Gerald <johngeraldcayabyab@gmail.com>
2022-01-14 23:56:44 +0800

    protecting routes

--- app/Routes/MeasurementRoute.php
+++ app/Routes/MeasurementRoute.php
@@ -9,12 +9,14 @@ class MeasurementRoute
 {
     public static function initialize()
     {
-        Route::get('measurements/{measurement}/slug', [MeasurementController::class, 'slug'])->name('measurements.slug');
-        Route::get('measurements/{measurement}', [MeasurementController::class, 'show'])->name('measurements.show');
-        Route::put('measurements/{measurement}', [MeasurementController::class, 'update'])->name('measurements.update');
-        Route::delete('measurements/{measurement}', [MeasurementController::class, 'destroy'])->name('measurements.destroy');
-        Route::post('measurements/mass_destroy', [MeasurementController::class, 'mass_destroy'])->name('measurements.mass_destroy');
-        Route::post('measurements', [MeasurementController::class, 'store'])->name('measurements.store');
-        Route::get('measurements', [MeasurementController::class, 'index'])->name('measurements.index');
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::get('measurements/{measurement}/slug', [MeasurementController::class, 'slug'])->name('measurements.slug');
+            Route::get('measurements/{measurement}', [MeasurementController::class, 'show'])->name('measurements.show');
+            Route::put('measurements/{measurement}', [MeasurementController::class, 'update'])->name('measurements.update');
+            Route::delete('measurements/{measurement}', [MeasurementController::class, 'destroy'])->name('measurements.destroy');
+            Route::post('measurements/mass_destroy', [MeasurementController::class, 'mass_destroy'])->name('measurements.mass_destroy');
+            Route::post('measurements', [MeasurementController::class, 'store'])->name('measurements.store');
+            Route::get('measurements', [MeasurementController::class, 'index'])->name('measurements.index');
+        });
     }
 }
