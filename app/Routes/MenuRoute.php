3574de6f573f60ef02a8634500b5d57e328ecbe3
Dlareg <johngeraldcayabyab@gmail.com>
2022-01-14 23:56:44 +0800

    protecting routes

--- app/Routes/MenuRoute.php
+++ app/Routes/MenuRoute.php
@@ -10,12 +10,14 @@ class MenuRoute
 {
     public static function initialize()
     {
-        Route::get('menus/{menu}/slug', [MenuController::class, 'slug'])->name('menus.slug');
-        Route::get('menus/{menu}', [MenuController::class, 'show'])->name('menus.show');
-        Route::put('menus/{menu}', [MenuController::class, 'update'])->name('menus.update');
-        Route::delete('menus/{menu}', [MenuController::class, 'destroy'])->name('menus.destroy');
-        Route::post('menus/mass_destroy', [MenuController::class, 'mass_destroy'])->name('menus.mass_destroy');
-        Route::post('menus', [MenuController::class, 'store'])->name('menus.store');
-        Route::get('menus', [MenuController::class, 'index'])->name('menus.index');
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::get('menus/{menu}/slug', [MenuController::class, 'slug'])->name('menus.slug');
+            Route::get('menus/{menu}', [MenuController::class, 'show'])->name('menus.show');
+            Route::put('menus/{menu}', [MenuController::class, 'update'])->name('menus.update');
+            Route::delete('menus/{menu}', [MenuController::class, 'destroy'])->name('menus.destroy');
+            Route::post('menus/mass_destroy', [MenuController::class, 'mass_destroy'])->name('menus.mass_destroy');
+            Route::post('menus', [MenuController::class, 'store'])->name('menus.store');
+            Route::get('menus', [MenuController::class, 'index'])->name('menus.index');
+        });
     }
 }
