f599bb7b167a4b7949e200544a85942fd72005c2
Gerald <johngeraldcayabyab@gmail.com>
2022-01-21 23:57:14 +0800

    improving fetch hook

--- resources/js/Hooks/useFetch.js
+++ resources/js/Hooks/useFetch.js
@@ -1,4 +1,5 @@
 import {getCookie} from "../Helpers/cookie";
+import {GET} from "../consts";
 
 const fetchData = () => {
     const controller = new AbortController();
@@ -12,17 +13,38 @@ const fetchData = () => {
     };
 
     return [
-        (url) => {
-            return fetch(url, {
+        (url, method, values = {}, headers = {}) => {
+            const fetchInit = {
                 headers: {
-                    ...defaultHeaders
+                    ...defaultHeaders,
+                    ...headers
                 },
-                signal
-            }).then((data) => {
-                return data.json();
+                signal,
+                method: method,
+            };
+            if (method === GET) {
+                values = Object.entries(values).map(e => e.join('=')).join('&');
+                url = `${url}?${values}`;
+            } else {
+                fetchInit.body = JSON.stringify(values);
+            }
+            return fetch(url, fetchInit).then((response) => {
+                if (response.ok) {
+                    return response;
+                }
+                throw response;
+            }).then((responseOk) => {
+                const contentType = responseOk.headers.get('Content-Type');
+                if (contentType === 'text/html; charset=UTF-8') {
+                    return responseOk.text();
+                } else if (contentType === 'application/json') {
+                    return responseOk.json();
+                } else {
+                    alert('Undefined response ok type!');
+                }
             });
         },
-        controller.abort.bind(controller) // notice binding context
+        controller.abort.bind(controller)
     ];
 };
 
