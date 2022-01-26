e550bce698d3fc8fed0702dfd9474877caa59aba
Gerald <johngeraldcayabyab@gmail.com>
2022-01-26 23:56:54 +0800

    added size checker in skeleton

--- resources/js/components/CustomInputSkeleton.js
+++ resources/js/components/CustomInputSkeleton.js
@@ -1,12 +1,12 @@
 import {Skeleton} from "antd";
 
-const CustomInputSkeleton = () => {
+const CustomInputSkeleton = (props) => {
     return (
         <Skeleton.Input
             className={'ant-skeleton-element-custom'}
             style={{display: 'block', width: '100%'}}
             active={true}
-            size={'small'}
+            size={props.size ? props.size : 'small'}
         />
     )
 };
