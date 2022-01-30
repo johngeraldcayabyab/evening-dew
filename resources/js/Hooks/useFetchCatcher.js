bf373744cb8f1cb38ca0331ab9c265b973ae0e8c
Dlareg <johngeraldcayabyab@gmail.com>
2022-01-30 23:29:09 +0800

    fixed error message handling in forms

--- resources/js/Hooks/useFetchCatcher.js
+++ resources/js/Hooks/useFetchCatcher.js
@@ -12,6 +12,16 @@ const useFetchCatcher = () => {
                 message.error('Please login first!');
             } else if (response.status === 403) {
                 message.error('You cant do this action! Please ask your admin for permission');
+            } else if (response.status === 422) {
+                return response.json().then((body) => {
+                    message.warning(body.message);
+                    return body.errors;
+                });
+            } else if (response.status === 500) {
+                return response.json().then((body) => {
+                    message.error(body.message);
+                    return body.errors;
+                });
             }
         },
         post: (response) => {
