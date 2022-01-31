8c97f86e17c30d4edb0e9a7b6f9cabd0de7fc38c
johngeraldcayabyab@gmail.com <johngeraldcayabyab@gmail.com>
2022-01-31 22:06:20 +0800

    added password confirmation checker

--- resources/js/Login/Login.js
+++ resources/js/Login/Login.js
@@ -86,17 +86,12 @@ const Login = () => {
                     <Form.Item name="remember" valuePropName="checked" noStyle>
                         <Checkbox>Remember me</Checkbox>
                     </Form.Item>
-
-                    <a className="login-form-forgot" href="">
-                        Forgot password
-                    </a>
                 </Form.Item>
 
                 <Form.Item>
                     <Button type="primary" htmlType="submit" className="login-form-button">
                         Log in
                     </Button>
-                    Or <a href="">register now!</a>
                 </Form.Item>
             </Form>
         </Card>
