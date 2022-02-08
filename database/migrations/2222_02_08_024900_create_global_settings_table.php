d4517468550b88450bf47b9cd1e0087082adca25
Gerald <johngeraldcayabyab@gmail.com>
2022-02-08 11:35:06 +0800

    Updated Seeders

---
+++ database/migrations/2222_02_08_024900_create_global_settings_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGlobalSettingsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('global_settings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('inventory_default_measurement_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('global_settings');
+    }
+}
---
+++ database/seeders/MeasurementSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Measurement;
+use Illuminate\Database\Seeder;
+
+class MeasurementSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            [
+                'name' => 'Units',
+                'type' => 'reference',
+                'ratio' => 1.00,
+                'rounding_precision' => 0.01,
+                'measurement_category_id' => 1,
+            ],
+            [
+                'name' => 'Dozens',
+                'type' => 'bigger',
+                'ratio' => 0.08,
+                'rounding_precision' => 0.01,
+                'measurement_category_id' => 1,
+            ],
+            [
+                'name' => 'kg',
+                'type' => 'reference',
+                'ratio' => 1.00,
+                'rounding_precision' => 0.01,
+                'measurement_category_id' => 2,
+            ],
+            [
+                'name' => 'g',
+                'type' => 'smaller',
+                'ratio' => 1000.00,
+                'rounding_precision' => 0.01,
+                'measurement_category_id' => 2,
+            ],
+            [
+                'name' => 'oz',
+                'type' => 'smaller',
+                'ratio' => 35.27,
+                'rounding_precision' => 0.01,
+                'measurement_category_id' => 2,
+            ],
+            [
+                'name' => 'lb',
+                'type' => 'smaller',
+                'ratio' => 2.20,
+                'rounding_precision' => 0.01,
+                'measurement_category_id' => 2,
+            ],
+        ];
+        foreach ($data as $datum) {
+            Measurement::create($datum);
+        }
+    }
+}
