e9b4c6b54840bf4023359723509584eecbdb5fe5
Dlareg <johngeraldcayabyab@gmail.com>
2022-02-10 08:49:58 +0800

    Created front end for countries

---
+++ resources/js/Country/CountryForm.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import {Form} from "antd";
+import {useParams} from "react-router-dom";
+import useFormState from "../Hooks/useFormState";
+import manifest from "./__manifest__.json";
+import FormButtons from "../components/FormButtons/FormButtons";
+import RowForm from "../components/Grid/RowForm";
+import ColForm from "../components/Grid/ColForm";
+import CustomForm from "../components/CustomForm";
+import FormItemText from "../components/FormItem/FormItemText";
+import ControlPanel from "../components/ControlPanel";
+import FormCard from "../components/FormCard";
+import FormItemSelectAjax from "../components/FormItem/FormItemSelectAjax";
+
+const CountryForm = () => {
+    let {id} = useParams();
+    const [form] = Form.useForm();
+    const [formState, formActions] = useFormState(id, form, manifest);
+    return (
+        <CustomForm
+            form={form}
+            onFinish={formActions.onFinish}
+        >
+            <ControlPanel
+                bottomColOneLeft={
+                    <FormButtons
+                        id={id}
+                        form={form}
+                        formState={formState}
+                        formActions={formActions}
+                        manifest={manifest}
+                    />
+                }
+            />
+            <FormCard {...formState}>
+                <RowForm>
+                    <ColForm>
+                        <FormItemText
+                            label={'Country Name'}
+                            name={'country_name'}
+                            message={'Please input country name'}
+                            required={true}
+                            {...formState}
+                        />
+
+                        <FormItemSelectAjax
+                            label={'Currency'}
+                            name={'currency_id'}
+                            url={'/api/currencies/option'}
+                            {...formState}
+                        />
+
+                        <FormItemText
+                            label={'Country Code'}
+                            name={'country_code'}
+                            {...formState}
+                        />
+                    </ColForm>
+
+                    <ColForm>
+                        <FormItemText
+                            label={'Country Calling Code'}
+                            name={'country_calling_code'}
+                            {...formState}
+                        />
+                    </ColForm>
+                </RowForm>
+            </FormCard>
+        </CustomForm>
+    );
+};
+
+
+export default CountryForm;
