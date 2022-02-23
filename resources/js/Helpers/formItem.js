9a02d0015b352b509c68afd84e508d9c8c3103a3
Gerald <johngeraldcayabyab@gmail.com>
2022-02-23 13:56:53 +0800

    Created a reusable wrapper and field props

---
+++ resources/js/Helpers/formItem.js
@@ -0,0 +1,41 @@
+import FormLabel from "../components/Typography/FormLabel";
+
+export const formItemFieldProps = (props, specialFieldProps = {}) => {
+    const formItemProps = {
+        label: props.label ? <FormLabel>{props.label}</FormLabel> : null,
+        name: props.name,
+        rules: [{required: props.required, message: props.message}],
+        colon: false,
+        labelCol: props.size === 'large' || props.size === 'medium' ? {span: 24} : {span: 8},
+        wrapperCol: props.size === 'large' || props.size === 'medium' ? {
+            span: 24,
+            style: {flex: '0 0 100%'}
+        } : {span: 16},
+    };
+
+    if (props.errors[props.name]) {
+        formItemProps.validateStatus = 'error';
+        formItemProps.help = props.errors[props.name];
+    } else if (props.errors[`${props.listName}.${props.groupName}.${props.name}`]) {
+        formItemProps.validateStatus = 'error';
+        formItemProps.help = props.errors[`${props.listName}.${props.groupName}.${props.name}`];
+    }
+
+    const fieldProps = {
+        disabled: props.formDisabled,
+        size: props.size ? props.size : 'small',
+        placeholder: props.placeholder ? props.placeholder : null,
+        ...specialFieldProps
+    };
+
+    if (props.isListField) {
+        formItemProps.isListField = true;
+        formItemProps.fieldKey = props.fieldKey;
+        delete formItemProps.labelCol;
+        formItemProps.wrapperCol = {span: 24};
+        formItemProps.style = props.style;
+        formItemProps.name = [props.groupName, props.name];
+    }
+
+    return [formItemProps, fieldProps];
+};
--- resources/js/components/FormItem/FormItemText.js
+++ resources/js/components/FormItem/FormItemText.js
@@ -1,41 +1,10 @@
-import FormLabel from "../Typography/FormLabel";
 import {Form, Input} from "antd";
 import React from "react";
 import CustomInputSkeleton from "../CustomInputSkeleton";
+import {formItemFieldProps} from "../../Helpers/formItem";
 
 const FormItemText = (props) => {
-
-    const formItemProps = {
-        label: props.label ? <FormLabel>{props.label}</FormLabel> : null,
-        name: props.name,
-        rules: [{required: props.required, message: props.message}],
-        colon: false,
-        labelCol: props.size === 'large' || props.size === 'medium' ? {span: 24} : {span: 8},
-        wrapperCol: props.size === 'large' || props.size === 'medium' ? {span: 24, style: {flex: '0 0 100%'}} : {span: 16},
-    };
-
-    if (props.errors[props.name]) {
-        formItemProps.validateStatus = 'error';
-        formItemProps.help = props.errors[props.name];
-    } else if (props.errors[`${props.listName}.${props.groupName}.${props.name}`]) {
-        formItemProps.validateStatus = 'error';
-        formItemProps.help = props.errors[`${props.listName}.${props.groupName}.${props.name}`];
-    }
-
-    const fieldProps = {
-        disabled: props.formDisabled,
-        size: props.size ? props.size : 'small',
-        placeholder: props.placeholder ? props.placeholder : null,
-    };
-
-    if (props.isListField) {
-        formItemProps.isListField = true;
-        formItemProps.fieldKey = props.fieldKey;
-        delete formItemProps.labelCol;
-        formItemProps.wrapperCol = {span: 24};
-        formItemProps.style = props.style;
-        formItemProps.name = [props.groupName, props.name];
-    }
+    const [formItemProps, fieldProps] = formItemFieldProps(props);
 
     return (
         <Form.Item {...formItemProps}>
