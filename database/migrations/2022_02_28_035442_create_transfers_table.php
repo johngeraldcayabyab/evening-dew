e4357ae2c68e85276e128767a398fe790b5253b3
Dlareg <johngeraldcayabyab@gmail.com>
2022-02-28 16:00:08 +0800

    creating transfer backend

---
+++ database/migrations/2022_02_28_035442_create_transfers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransfersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->id();
+            $table->string('number')->nullable();
+            $table->bigInteger('contact_id')->nullable();
+            $table->bigInteger('operation_type_id')->nullable();
+            $table->bigInteger('source_location_id')->nullable();
+            $table->bigInteger('destination_location_id')->nullable();
+            $table->dateTime('scheduled_date');
+            $table->string('source_document')->nullable();
+
+            $table->string('tracking_reference')->nullable();
+            $table->unsignedDouble('weight');
+            $table->unsignedDouble('weight_for_shipping');
+
+            $table->string('shipping_policy')->nullable();
+            $table->bigInteger('responsible_id')->nullable();
+
+            $table->string('note')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('transfers');
+    }
+}
