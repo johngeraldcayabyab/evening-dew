3586fc48de06082d742008524d91c5e9616f8f7f
Gerald <johngeraldcayabyab@gmail.com>
2022-03-02 11:51:01 +0800

    80% fixed sales order lines

--- resources/js/components/FormItem/FormItemSelectAjax.js
+++ resources/js/components/FormItem/FormItemSelectAjax.js
@@ -31,22 +31,32 @@ const FormItemSelectAjax = (props) => {
 
     useEffect(() => {
         if (!props.initialLoad) {
-            let val = objectHasValue(props.initialValues);
-            if (val) {
+            let val = null;
+            if (objectHasValue(props.initialValues)) {
                 val = getQueryFromInitialValue();
             }
             getOptions(val);
+            console.log('is initial load trigger');
         }
     }, [props.initialLoad]);
 
     useEffect(() => {
+        /**
+         * The issue is because this is not working
+         * and the removed sales order lines is getting re-queried because it's
+         * still part of the initialValues
+         */
         if (props.search) {
             getOptions(props.search);
+            console.log('is search trigger', props.search);
         }
     }, [props.search]);
 
     function getQueryFromInitialValue() {
         let search = props.initialValues;
+        /**
+         * should be called "initial search" instead of "query"
+         */
         props.query.split('.').forEach((query) => {
             if (search && query in search) {
                 search = search[query];
@@ -54,6 +64,27 @@ const FormItemSelectAjax = (props) => {
                 search = null;
             }
         });
+        /**
+         * Okay now we know it's a list field
+         *
+         * There are three types of initial values for list fields
+         *
+         * 1) One is the "main" outside the fields object
+         * 2) Second is for "create" mode and "inside" the fields object (rare but possible)
+         * 3) Third is the "edit" mode that gives it actual values
+         *
+         */
+        if ((props.isListField && props.id && !props.formDisabled) || (props.isListField && !props.id && !props.formDisabled)) {
+            search = props.initialValues;
+            props.query.split('.').slice(-2).forEach((query) => {
+                if (search && query in search) {
+                    search = search[query];
+                } else {
+                    search = null;
+                }
+            });
+        }
+
         return search;
     }
 
