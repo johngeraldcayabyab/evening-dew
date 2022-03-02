eeedcdc89b16e2d42762c507c10c73d1543eb63c
Dlareg <johngeraldcayabyab@gmail.com>
2022-03-03 07:55:29 +0800

    making transaction line fields reusable

--- database/migrations/2022_02_11_064344_create_sales_orders_table.php
+++ database/migrations/2022_02_11_064344_create_sales_orders_table.php
@@ -10,23 +10,23 @@ class CreateSalesOrdersTable extends Migration
     {
         Schema::create('sales_orders', function (Blueprint $table) {
             $table->id();
-            $table->string('number');
+            $table->string('number')->nullable();
             $table->bigInteger('customer_id')->nullable();
             $table->bigInteger('invoice_address_id')->nullable();
             $table->bigInteger('delivery_address_id')->nullable();
 
             $table->dateTime('expiration')->nullable();
-            $table->dateTime('quotation_date');
+            $table->dateTime('quotation_date')->nullable();
             $table->bigInteger('payment_term_id')->nullable();
 
             $table->bigInteger('salesperson_id')->nullable();
-            $table->string('customer_reference');
+            $table->string('customer_reference')->nullable();
 
             $table->string('shipping_policy')->nullable();
             $table->dateTime('expected_delivery_date')->nullable();
 
 
-            $table->string('source_document');
+            $table->string('source_document')->nullable();
 
             $table->softDeletes();
             $table->timestamps();
