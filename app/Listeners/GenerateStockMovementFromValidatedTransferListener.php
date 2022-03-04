795a6ced5d956ffdb860b13642e0fbd23ff62176
Gerald <johngeraldcayabyab@gmail.com>
2022-03-04 10:28:16 +0800

    Transfer now generates stock movement on validation

---
+++ app/Listeners/GenerateStockMovementFromValidatedTransferListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\TransferValidatedEvent;
+use App\Models\StockMovement;
+use App\Models\TransferLine;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class GenerateStockMovementFromValidatedTransferListener implements ShouldQueue
+{
+    public function handle(TransferValidatedEvent $event)
+    {
+        $transfer = $event->transfer;
+        $transferLines = $transfer->transferLines;
+        $stockMovementData = [];
+        foreach ($transferLines as $transferLine) {
+            $stockMovementData[] = [
+                'reference' => $transfer->reference,
+                'source' => $transfer->reference,
+                'product_id' => $transferLine->product_id,
+                'source_location_id' => $transfer->source_location_id,
+                'destination_location_id' => $transfer->destination_location_id,
+                'quantity_done' => $transferLine->demand,
+            ];
+        }
+        if (count($stockMovementData)) {
+            StockMovement::insertMany($stockMovementData);
+        }
+    }
+}
