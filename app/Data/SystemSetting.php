4fa1a498587c61a8b95eb336cb272d6bc2f3fb57
Dlareg <johngeraldcayabyab@gmail.com>
2022-03-10 09:39:17 +0800

    creating dynamic routes generator

--- app/Data/SystemSetting.php
+++ app/Data/SystemSetting.php
@@ -7,4 +7,28 @@ class SystemSetting
     const OPTION_LIMIT = 10;
     const PAGE_SIZE = 80;
     const DATE_TIME_FORMAT = 'Y-m-d H:i:s';
+    const INITIAL_VALUES = 'initial_values';
+    const OPTION = 'option';
+    const SLUG = 'slug';
+    const SHOW = 'show';
+    const UPDATE = 'update';
+    const DESTROY = 'destroy';
+    const MASS_DESTROY = 'mass_destroy';
+    const STORE = 'store';
+    const INDEX = 'index';
+
+    public static function getRoutes()
+    {
+        return [
+            self::INITIAL_VALUES,
+            self::OPTION,
+            self::SLUG,
+            self::SHOW,
+            self::UPDATE,
+            self::DESTROY,
+            self::MASS_DESTROY,
+            self::STORE,
+            self::INDEX
+        ];
+    }
 }
---
+++ app/Services/RouteGenerator.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Services;
+
+use App\Contacts\Generator;
+use App\Data\SystemSetting;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+use ReflectionClass;
+
+class RouteGenerator implements Generator
+{
+    private static $plural;
+    private static $singular;
+
+    public function __construct($model)
+    {
+        $model = new $model;
+        self::$plural = $model->getTable();
+        self::$singular = Str::snake(Str::replace('App\Models\\', '', get_class($model)));
+    }
+
+    public static function generate($controller)
+    {
+        Route::middleware('auth:sanctum')->group(function () use ($controller) {
+            $f = new ReflectionClass($controller);
+            $methods = $f->getMethods();
+            $plural = self::$plural;
+            $singular = self::$singular;
+            $initialValues = SystemSetting::INITIAL_VALUES;
+            $option = SystemSetting::OPTION;
+            $slug = SystemSetting::SLUG;
+            $show = SystemSetting::SHOW;
+            $update = SystemSetting::UPDATE;
+            $destroy = SystemSetting::DESTROY;
+            $massDestroy = SystemSetting::MASS_DESTROY;
+            $store = SystemSetting::STORE;
+            $index = SystemSetting::INDEX;
+            $controllerInstance = new $controller;
+            if (self::findMethod($methods, SystemSetting::INITIAL_VALUES, $controllerInstance)) {
+                Route::get("{$plural}/{$initialValues}", [$controller, $initialValues])->name("{$plural}.{$initialValues}");
+            }
+            if (self::findMethod($methods, SystemSetting::OPTION, $controllerInstance)) {
+                Route::get("{$plural}/{$option}", [$controller, $option])->name("{$plural}.{$option}");
+            }
+            if (self::findMethod($methods, SystemSetting::SLUG, $controllerInstance)) {
+                Route::get("{$plural}/{{$singular}}/{$slug}", [$controller, $slug])->name("{$plural}.{$slug}");
+            }
+            if (self::findMethod($methods, SystemSetting::SHOW, $controllerInstance)) {
+                Route::get("{$plural}/{{$singular}}", [$controller, $show])->name("{$plural}.{$show}");
+            }
+            if (self::findMethod($methods, SystemSetting::UPDATE, $controllerInstance)) {
+                Route::put("{$plural}/{{$singular}}", [$controller, $update])->name("{$plural}.{$update}");
+            }
+            if (self::findMethod($methods, SystemSetting::DESTROY, $controllerInstance)) {
+                Route::delete("{$plural}/{{$singular}}", [$controller, $destroy])->name("{$plural}.{$destroy}");
+            }
+            if (self::findMethod($methods, SystemSetting::MASS_DESTROY, $controllerInstance)) {
+                Route::post("{$plural}/{$massDestroy}", [$controller, $massDestroy])->name("{$plural}.{$massDestroy}");
+            }
+            if (self::findMethod($methods, SystemSetting::STORE, $controllerInstance)) {
+                Route::post("{$plural}", [$controller, $store])->name("{$plural}.{$store}");
+            }
+            if (self::findMethod($methods, SystemSetting::INDEX, $controllerInstance)) {
+                Route::get("{$plural}", [$controller, $index])->name("{$plural}.{$index}");
+            }
+        });
+    }
+
+    private static function findMethod($methods, $specificMethod, $controllerInstance)
+    {
+        foreach ($methods as $method) {
+            if ($method->class == get_class($controllerInstance)) {
+                if ($method->name === $specificMethod) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
