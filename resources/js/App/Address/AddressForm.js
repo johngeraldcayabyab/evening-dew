bc46dd26bfb35f7cf56df89dc8a90bac3e7abf53
Dlareg <johngeraldcayabyab@gmail.com>
2022-03-15 13:17:05 +0800

    cleanup

--- resources/js/App/Address/AddressForm.js
+++ resources/js/App/Address/AddressForm.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {Form} from "antd";
 import {useParams} from "react-router-dom";
 import useFormState from "../../Hooks/useFormState";
