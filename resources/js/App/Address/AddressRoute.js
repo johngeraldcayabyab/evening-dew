7b6e68ecbbe9f09d2d01313c581ed3f7d8671226
Gerald <johngeraldcayabyab@gmail.com>
2022-03-15 11:32:54 +0800

    breadcrumbs working

--- resources/js/App/Address/AddressRoute.js
+++ resources/js/App/Address/AddressRoute.js
@@ -3,20 +3,21 @@ import React from "react";
 import manifest from "./__manifest__.json";
 import AddressList from "./AddressList";
 import AddressForm from "./AddressForm";
+import {uuidv4} from "../../Helpers/string";
 
 const AddressRoute = () => {
     const moduleName = manifest.moduleName;
     return (
         <Switch>
-            <Route exact key={`${moduleName}_table`} path={`/${moduleName}`}>
-                <AddressList/>
-            </Route>
-            <Route exact key={`${moduleName}_create`} path={`/${moduleName}/create`}>
-                <AddressForm/>
-            </Route>
-            <Route exact key={`${moduleName}_update`} path={`/${moduleName}/:id`}>
-                <AddressForm/>
-            </Route>
+            <Route exact key={uuidv4()} path={`/${moduleName}`}
+                   render={props => <AddressList key={props.location.key}/>}
+            />
+            <Route exact key={uuidv4()} path={`/${moduleName}/create`}
+                   render={props => <AddressForm key={props.location.key}/>}
+            />
+            <Route exact key={uuidv4()} path={`/${moduleName}/:id`}
+                   render={props => <AddressForm key={props.location.key}/>}
+            />
         </Switch>
     );
 };
