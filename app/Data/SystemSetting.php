9533a9976149deb87c659913fcaa2b4ededadb3c
Dlareg <johngeraldcayabyab@gmail.com>
2022-03-16 13:59:47 +0800

    removed option in route generations

--- app/Data/SystemSetting.php
+++ app/Data/SystemSetting.php
@@ -8,7 +8,6 @@ class SystemSetting
     const TIME_STAMP_FORMAT = 'm/d/Y h:i:s A';
     const DATE_TIME_FORMAT = 'Y-m-d H:i:s';
     const INITIAL_VALUES = 'initial_values';
-    const OPTION = 'option';
     const SLUG = 'slug';
     const SHOW = 'show';
     const UPDATE = 'update';
@@ -16,19 +15,4 @@ class SystemSetting
     const MASS_DESTROY = 'mass_destroy';
     const STORE = 'store';
     const INDEX = 'index';
-
-    public static function getRoutes()
-    {
-        return [
-            self::INITIAL_VALUES,
-            self::OPTION,
-            self::SLUG,
-            self::SHOW,
-            self::UPDATE,
-            self::DESTROY,
-            self::MASS_DESTROY,
-            self::STORE,
-            self::INDEX
-        ];
-    }
 }
