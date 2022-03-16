7ceab5c6909b4ee0afdc8f7a89591dae3875f797
Gerald <johngeraldcayabyab@gmail.com>
2022-03-16 11:13:22 +0800

    persist app menu

--- resources/js/Helpers/breadcrumbs.js
+++ resources/js/Helpers/breadcrumbs.js
@@ -1,12 +1,3 @@
-export const addBreadcrumbs = (breadcrumb) => {
-    let breadcrumbs = [];
-    if (localStorage.getItem("breadcrumbs")) {
-        breadcrumbs = JSON.parse(localStorage.getItem("breadcrumbs"));
-    }
-    breadcrumbs.push(breadcrumb);
-    localStorage.setItem("breadcrumbs", JSON.stringify(breadcrumbs));
-};
-
 export const getBreadcrumbs = () => {
     let breadcrumbs = [];
     if (localStorage.getItem("breadcrumbs")) {
