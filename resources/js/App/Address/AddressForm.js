ee8157c9ed56aeb2d9737c05b5937cb8dcb1c710
Dlareg <johngeraldcayabyab@gmail.com>
2022-03-21 13:50:55 +0800

    Address working

--- resources/js/App/Address/AddressForm.js
+++ resources/js/App/Address/AddressForm.js
@@ -13,123 +13,122 @@ import FormCard from "../../Components/FormCard";
 import FormItemSelectAjax from "../../Components/FormItem/FormItemSelectAjax";
 import FormItemSelect from "../../Components/FormItem/FormItemSelect";
 import CustomBreadcrumb from "../../Components/CustomBreadcrumb";
+import {FormContextProvider} from "../../Contexts/FormContext";
 
 const AddressForm = () => {
     let {id} = useParams();
     const [form] = Form.useForm();
     const [formState, formActions] = useFormHook(id, form, manifest, true);
 
-
     return (
-        <CustomForm
-            form={form}
-            onFinish={formActions.onFinish}
-        >
-            <ControlPanel
-                topColOneLeft={<CustomBreadcrumb formState={formState}/>}
-                bottomColOneLeft={
-                    <FormButtons
-                        id={id}
-                        form={form}
-                        formState={formState}
-                        formActions={formActions}
-                        manifest={manifest}
-                    />
-                }
-            />
-            <FormCard {...formState}>
-                <RowForm>
-                    <ColForm>
-                        <FormItemText
+        <FormContextProvider value={{form: form, onFinish: formActions.onFinish}}>
+            <CustomForm>
+                <ControlPanel
+                    topColOneLeft={<CustomBreadcrumb formState={formState}/>}
+                    bottomColOneLeft={
+                        <FormButtons
+                            id={id}
                             form={form}
-                            label={'Address Name'}
-                            name={'address_name'}
-                            message={'Please input address name'}
-                            required={true}
-                            size={'large'}
-                            {...formState}
+                            formState={formState}
+                            formActions={formActions}
+                            manifest={manifest}
                         />
-                    </ColForm>
-                </RowForm>
+                    }
+                />
+                <FormCard {...formState}>
+                    <RowForm>
+                        <ColForm>
+                            <FormItemText
+                                form={form}
+                                label={'Address Name'}
+                                name={'address_name'}
+                                message={'Please input address name'}
+                                required={true}
+                                size={'large'}
+                                {...formState}
+                            />
+                        </ColForm>
+                    </RowForm>
 
-                <RowForm>
-                    <ColForm>
-                        <FormItemText
-                            form={form}
-                            label={'Street 1'}
-                            name={'street_one'}
-                            {...formState}
-                        />
+                    <RowForm>
+                        <ColForm>
+                            <FormItemText
+                                form={form}
+                                label={'Street 1'}
+                                name={'street_one'}
+                                {...formState}
+                            />
 
-                        <FormItemText
-                            form={form}
-                            label={'Street 2'}
-                            name={'street_two'}
-                            {...formState}
-                        />
+                            <FormItemText
+                                form={form}
+                                label={'Street 2'}
+                                name={'street_two'}
+                                {...formState}
+                            />
 
-                        <FormItemText
-                            form={form}
-                            label={'City'}
-                            name={'city'}
-                            {...formState}
-                        />
+                            <FormItemText
+                                form={form}
+                                label={'City'}
+                                name={'city'}
+                                {...formState}
+                            />
 
-                        <FormItemText
-                            form={form}
-                            label={'State'}
-                            name={'state'}
-                            {...formState}
-                        />
+                            <FormItemText
+                                form={form}
+                                label={'State'}
+                                name={'state'}
+                                {...formState}
+                            />
 
-                        <FormItemText
-                            form={form}
-                            label={'Zip'}
-                            name={'zip'}
-                            {...formState}
-                        />
+                            <FormItemText
+                                form={form}
+                                label={'Zip'}
+                                name={'zip'}
+                                {...formState}
+                            />
 
-                        <FormItemSelectAjax
-                            form={form}
-                            label={'Country'}
-                            name={'country_id'}
-                            url={'/api/countries'}
-                            {...formState}
-                            query={'country.country_name'}
-                        />
-                    </ColForm>
+                            <FormItemSelectAjax
+                                form={form}
+                                label={'Country'}
+                                name={'country_id'}
+                                url={'/api/countries'}
+                                {...formState}
+                                query={'country.country_name'}
+                            />
+                        </ColForm>
 
-                    <ColForm>
-                        <FormItemSelectAjax
-                            form={form}
-                            label={'Contact'}
-                            name={'contact_id'}
-                            message={'Please select a contact'}
-                            required={true}
-                            url={'/api/contacts'}
-                            {...formState}
-                            query={'contact.name'}
-                        />
+                        <ColForm>
+                            <FormItemSelectAjax
+                                form={form}
+                                label={'Contact'}
+                                name={'contact_id'}
+                                message={'Please select a contact'}
+                                required={true}
+                                url={'/api/contacts'}
+                                {...formState}
+                                query={'contact.name'}
+                            />
 
-                        <FormItemSelect
-                            form={form}
-                            label={'Type'}
-                            name={'type'}
-                            message={'Please select an address type'}
-                            required={true}
-                            options={[
-                                {value: 'default', label: 'Default'},
-                                {value: 'invoice', label: 'Invoice'},
-                                {value: 'delivery', label: 'Delivery'},
-                                {value: 'others', label: 'Others'},
-                                {value: 'private', label: 'Private'},
-                            ]}
-                            {...formState}
-                        />
-                    </ColForm>
-                </RowForm>
-            </FormCard>
-        </CustomForm>
+                            <FormItemSelect
+                                form={form}
+                                label={'Type'}
+                                name={'type'}
+                                message={'Please select an address type'}
+                                required={true}
+                                options={[
+                                    {value: 'default', label: 'Default'},
+                                    {value: 'invoice', label: 'Invoice'},
+                                    {value: 'delivery', label: 'Delivery'},
+                                    {value: 'others', label: 'Others'},
+                                    {value: 'private', label: 'Private'},
+                                ]}
+                                {...formState}
+                            />
+                        </ColForm>
+                    </RowForm>
+                </FormCard>
+            </CustomForm>
+        </FormContextProvider>
     );
 };
 
