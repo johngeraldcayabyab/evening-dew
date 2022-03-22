6d8aa213b02377508cd4a3e47c5c7c59555675fa
Gerald <johngeraldcayabyab@gmail.com>
2022-03-22 13:36:06 +0800

    warehouse added

--- resources/js/Modules/Warehouse/WarehouseForm.js
+++ resources/js/Modules/Warehouse/WarehouseForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Divider, Form, Tabs} from "antd";
 import {useParams} from "react-router-dom";
 import useFormHook from "../../Hooks/useFormHook";
@@ -10,10 +10,11 @@ import CustomForm from "../../Components/CustomForm";
 import ControlPanel from "../../Components/ControlPanel";
 import FormCard from "../../Components/FormCard";
 import FormItemText from "../../Components/FormItem/FormItemText";
-import FormItemSelectAjax from "../../Components/FormItem/FormItemSelectAjax";
 import FormItemCheckbox from "../../Components/FormItem/FormItemCheckbox";
 import CustomBreadcrumb from "../../Components/CustomBreadcrumb";
 import {FormContextProvider} from "../../Contexts/FormContext";
+import useOptionHook from "../../Hooks/useOptionHook";
+import FormItemSelectTest from "../../Components/FormItem/FormItemSelectTest";
 
 const {TabPane} = Tabs;
 
@@ -21,6 +22,42 @@ const WarehouseForm = () => {
     let {id} = useParams();
     const [form] = Form.useForm();
     const [formState, formActions] = useFormHook(id, form, manifest, true);
+
+    const viewLocationOptions = useOptionHook('/api/locations', 'view_location.name');
+    const stockLocationOptions = useOptionHook('/api/locations', 'stock_location.name');
+    const inputLocationOptions = useOptionHook('/api/locations', 'input_location.name');
+    const qualityControlLocationOptions = useOptionHook('/api/locations', 'quality_control_location.name');
+    const packingLocationOptions = useOptionHook('/api/locations', 'packing_location.name');
+    const outputLocationOptions = useOptionHook('/api/locations', 'output_location.name');
+    const stockAfterManufacturingLocationOptions = useOptionHook('/api/locations', 'stock_after_manufacturing_location.name');
+    const pickingBeforeManufacturingLocationOptions = useOptionHook('/api/locations', 'picking_before_manufacturing_location.name');
+    const inTypeOptions = useOptionHook('/api/operations_types', 'in_type.name');
+    const internalTypeOptions = useOptionHook('/api/operations_types', 'internal_type.name');
+    const pickTypeOptions = useOptionHook('/api/operations_types', 'pick_type.name');
+    const packTypeOptions = useOptionHook('/api/operations_types', 'pack_type.name');
+    const outTypeOptions = useOptionHook('/api/operations_types', 'out_type.name');
+    const stockAfterManufacturingOperationTypeOptions = useOptionHook('/api/operations_types', 'stock_after_manufacturing_operation_type.name');
+    const pickingBeforeManufacturingOperationTypeOptions = useOptionHook('/api/operations_types', 'picking_before_manufacturing_operation_type.name');
+    const manufacturingOperationTypeOperationTypeOptions = useOptionHook('/api/operations_types', 'manufacturing_operation_type.name');
+
+    useEffect(() => {
+        viewLocationOptions.getInitialOptions(formState);
+        stockLocationOptions.getInitialOptions(formState);
+        inputLocationOptions.getInitialOptions(formState);
+        qualityControlLocationOptions.getInitialOptions(formState);
+        packingLocationOptions.getInitialOptions(formState);
+        outputLocationOptions.getInitialOptions(formState);
+        stockAfterManufacturingLocationOptions.getInitialOptions(formState);
+        pickingBeforeManufacturingLocationOptions.getInitialOptions(formState);
+        inTypeOptions.getInitialOptions(formState);
+        internalTypeOptions.getInitialOptions(formState);
+        pickTypeOptions.getInitialOptions(formState);
+        packTypeOptions.getInitialOptions(formState);
+        outTypeOptions.getInitialOptions(formState);
+        stockAfterManufacturingOperationTypeOptions.getInitialOptions(formState);
+        pickingBeforeManufacturingOperationTypeOptions.getInitialOptions(formState);
+        manufacturingOperationTypeOperationTypeOptions.getInitialOptions(formState);
+    }, [formState.initialLoad]);
 
     return (
         <FormContextProvider
@@ -84,76 +121,68 @@ const WarehouseForm = () => {
                                         <Divider orientation={'left'}>
                                             Locations
                                         </Divider>
-                                        <FormItemSelectAjax
+                                        <FormItemSelectTest
                                             label={'View Location'}
                                             name={'view_location_id'}
                                             message={'Please select a view location'}
                                             required={true}
-                                            url={'/api/locations'}
-                                            query={'view_location.name'}
+                                            {...viewLocationOptions}
                                         />
 
-                                        <FormItemSelectAjax
+                                        <FormItemSelectTest
                                             label={'Stock Location'}
                                             name={'stock_location_id'}
                                             message={'Please select a stock location'}
                                             required={true}
-                                            url={'/api/locations'}
-                                            query={'stock_location.name'}
+                                            {...stockLocationOptions}
                                         />
 
-                                        <FormItemSelectAjax
+                                        <FormItemSelectTest
                                             label={'Input Location'}
                                             name={'input_location_id'}
                                             message={'Please select a input location'}
                                             required={true}
-                                            url={'/api/locations'}
-                                            query={'input_location.name'}
+                                            {...inputLocationOptions}
                                         />
 
-                                        <FormItemSelectAjax
+                                        <FormItemSelectTest
                                             label={'Quality Control Location'}
                                             name={'quality_control_location_id'}
                                             message={'Please select a quality control location'}
                                             required={true}
-                                            url={'/api/locations'}
-                                            query={'quality_control_location.name'}
+                                            {...qualityControlLocationOptions}
                                         />
 
-                                        <FormItemSelectAjax
+                                        <FormItemSelectTest
                                             label={'Packing Location'}
                                             name={'packing_location_id'}
                                             message={'Please select a packing location'}
                                             required={true}
-                                            url={'/api/locations'}
-                                            query={'packing_location.name'}
+                                            {...packingLocationOptions}
                                         />
 
-                                        <FormItemSelectAjax
+                                        <FormItemSelectTest
                                             label={'Output Location'}
                                             name={'output_location_id'}
                                             message={'Please select a output location'}
                                             required={true}
-                                            url={'/api/locations'}
-                                            query={'output_location.name'}
+                                            {...outputLocationOptions}
                                         />
 
-                                        <FormItemSelectAjax
+                                        <FormItemSelectTest
                                             label={'Stock after manufacturing location'}
                                             name={'stock_after_manufacturing_location_id'}
                                             message={'Please select a stock after manufacturing location'}
                                             required={true}
-                                            url={'/api/locations'}
-                                            query={'stock_after_manufacturing_location.name'}
+                                            {...stockAfterManufacturingLocationOptions}
                                         />
 
-                                        <FormItemSelectAjax
+                                        <FormItemSelectTest
                                             label={'Picking before manufacturing location'}
                                             name={'picking_before_manufacturing_location_id'}
                                             message={'Please select a picking before manufacturing location'}
                                             required={true}
-                                            url={'/api/locations'}
-                                            query={'picking_before_manufacturing_location.name'}
+                                            {...pickingBeforeManufacturingLocationOptions}
                                         />
                                     </ColForm>
 
@@ -162,69 +191,61 @@ const WarehouseForm = () => {
                                             Operation Types
                                         </Divider>
 
-                                        <FormItemSelectAjax
+                                        <FormItemSelectTest
                                             label={'In Type'}
                                             name={'in_type_id'}
                                             message={'Please select a in operation type'}
                                             required={true}
-                                            url={'/api/operations_types'}
-                                            query={'in_type.name'}
+                                            {...inTypeOptions}
                                         />
-                                        <FormItemSelectAjax
+                                        <FormItemSelectTest
                                             label={'Internal Type'}
                                             name={'internal_type_id'}
                                             message={'Please select a internal operation type'}
                                             required={true}
-                                            url={'/api/operations_types'}
-                                            query={'internal_type.name'}
+                                            {...internalTypeOptions}
                                         />
-                                        <FormItemSelectAjax
+                                        <FormItemSelectTest
                                             label={'Pick Type'}
                                             name={'pick_type_id'}
                                             message={'Please select a pick operation type'}
                                             required={true}
-                                            url={'/api/operations_types'}
-                                            query={'pick_type.name'}
+                                            {...pickTypeOptions}
                                         />
-                                        <FormItemSelectAjax
+                                        <FormItemSelectTest
                                             label={'Pack Type'}
                                             name={'pack_type_id'}
                                             message={'Please select a pack operation type'}
                                             required={true}
-                                            url={'/api/operations_types'}
-                                            query={'pack_type.name'}
+                                            {...packTypeOptions}
                                         />
-                                        <FormItemSelectAjax
+                                        <FormItemSelectTest
                                             label={'Out Type'}
                                             name={'out_type_id'}
                                             message={'Please select a out operation type'}
                                             required={true}
-                                            url={'/api/operations_types'}
-                                            query={'out_type.name'}
+                                            {...outTypeOptions}
                                         />
-                                        <FormItemSelectAjax
+                                        <FormItemSelectTest
                                             label={'Stock After Manufacturing Operation Type'}
                                             name={'stock_after_manufacturing_operation_type_id'}
                                             message={'Please select a stock after manufacturing operation type'}
                                             required={true}
-                                            url={'/api/operations_types'}
-                                            query={'stock_after_manufacturing_operation_type.name'}
+                                            {...stockAfterManufacturingOperationTypeOptions}
                                         />
-                                        <FormItemSelectAjax
+                                        <FormItemSelectTest
                                             label={'Picking Before Manufacturing Operation Type'}
                                             name={'picking_before_manufacturing_operation_type_id'}
                                             message={'Please select a picking before manufacturing operation type'}
                                             required={true}
-                                            url={'/api/operations_types'}
-                                            query={'picking_before_manufacturing_operation_type.name'}
+                                            {...pickingBeforeManufacturingOperationTypeOptions}
                                         />
-                                        <FormItemSelectAjax
+                                        <FormItemSelectTest
                                             label={'Manufacturing Operation Type'}
                                             name={'manufacturing_operation_type_id'}
                                             message={'Please select a manufacturing operation type'}
                                             required={true}
-                                            url={'/api/operations_types'}
-                                            query={'manufacturing_operation_type.name'}
+                                            {...manufacturingOperationTypeOperationTypeOptions}
                                         />
                                     </ColForm>
                                 </RowForm>
