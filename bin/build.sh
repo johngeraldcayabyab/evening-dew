f810d5d10eb91693e8a90b298ebc570299637c6a
Gerald <johngeraldcayabyab@gmail.com>
2022-03-25 14:35:10 +0800

    basic zipping and dependency installation done

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
-BUILD_NAME="build_$(date "+%Y.%m.%d_%H.%M.%S")"
+BUILD_FOLDER="build_$(date "+%Y.%m.%d_%H.%M.%S")"
 
-mkdir "../builds/${BUILD_NAME}"
+mkdir "./builds/${BUILD_FOLDER}"
+
+# shellcheck disable=SC2164
+cd "./builds/${BUILD_FOLDER}";  git clone --depth 1 git@github.com:johngeraldcayabyab/evening-dew.git ./
+
+pwd
+
+cd ../../
+
+composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+npm run production
+
+pwd
+
+BUILD_FOLDER_ZIP="${BUILD_FOLDER}.zip"
+
+zip -r "./builds/${BUILD_FOLDER_ZIP}" "./builds/${BUILD_FOLDER}"
+
+rm -rf "./builds/${BUILD_FOLDER}"
+
+
