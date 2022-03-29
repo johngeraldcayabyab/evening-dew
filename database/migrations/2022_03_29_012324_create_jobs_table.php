d664d9a4202ae348a25f6d18eba1ce6e71cac676
Gerald <johngeraldcayabyab@gmail.com>
2022-03-29 10:26:55 +0800

    refactoring time

--- database/migrations/2022_03_29_012324_create_jobs_table.php
+++ database/migrations/2022_03_29_012324_create_jobs_table.php
@@ -11,6 +11,7 @@ class CreateJobsTable extends Migration
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
             $table->string('job');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
