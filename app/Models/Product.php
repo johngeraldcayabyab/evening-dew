cc44cc5ed5694aa6cf6ba2642aaee31460fb14b2
Gerald <johngeraldcayabyab@gmail.com>
2022-03-30 11:26:26 +0800

    readded quantity

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -66,6 +66,25 @@ class Product extends Model implements Sluggable
         return $this->hasOne(Material::class);
     }
 
+    public function updateQuantity($operationType, $demand, $materialQuantity = null)
+    {
+        if ($operationType->type === OperationType::DELIVERY) {
+            if ($materialQuantity) {
+                $this->quantity = $this->quantity - ($materialQuantity * $demand);
+            } else {
+                $this->quantity = $this->quantity - $demand;
+            }
+        } else if ($operationType->type === OperationType::RECEIPT) {
+            if ($materialQuantity) {
+                $this->quantity = $this->quantity + ($materialQuantity * $demand);
+            } else {
+                $this->quantity = $this->quantity + $demand;
+            }
+        }
+        $this->save();
+        return $this;
+    }
+
     public function slug()
     {
         return 'name';
