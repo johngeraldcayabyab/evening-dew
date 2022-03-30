c6d85fb386ae2b279d1acb62276e43e4c141f84f
Dlareg <johngeraldcayabyab@gmail.com>
2022-03-30 15:28:29 +0800

    createed adjustment listener

--- app/Listeners/GenerateStockMovementFromValidatedAdjustment.php
+++ app/Listeners/GenerateStockMovementFromValidatedAdjustment.php
@@ -3,6 +3,9 @@
 namespace App\Listeners;
 
 use App\Events\AdjustmentValidatedEvent;
+use App\Events\ProductHasMaterialEvent;
+use App\Models\Product;
+use App\Models\StockMovement;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
 class GenerateStockMovementFromValidatedAdjustment implements ShouldQueue
@@ -10,5 +13,39 @@ class GenerateStockMovementFromValidatedAdjustment implements ShouldQueue
     public function handle(AdjustmentValidatedEvent $event)
     {
         $adjustment = $event->adjustment;
+        $adjustmentLines = $adjustment->adjustmentLines;
+        $warehouse = $adjustment->warehouse;
+        $operationType = $warehouse->adjustmentOperationType;
+        $stockMovementData = [];
+        foreach ($adjustmentLines as $adjustmentLine) {
+            $adjustmentLineProduct = $adjustmentLine->product;
+            if (Product::isStorable($adjustmentLineProduct->product_type)) {
+                $sourceLocationId = null;
+                $destinationLocationId = null;
+                $quantityDone = $adjustmentLine->quantity_on_hand - $adjustmentLine->quantity_counted;
+                if ($adjustmentLine->quantity_on_hand > $adjustmentLine->quantity_counted) {
+                    $sourceLocationId = $warehouse->stock_location_id;
+                    $destinationLocationId = $warehouse->adjustment_location_id;
+                }
+                if ($adjustmentLine->quantity_on_hand < $adjustmentLine->quantity_counted) {
+                    $sourceLocationId = $warehouse->adjustment_location_id;
+                    $destinationLocationId = $warehouse->stock_location_id;
+                }
+                $stockMovementData[] = [
+                    'reference' => $adjustment->number,
+                    'source' => $adjustment->number,
+                    'product_id' => $adjustmentLine->product_id,
+                    'source_location_id' => $sourceLocationId,
+                    'destination_location_id' => $destinationLocationId,
+                    'quantity_done' => $quantityDone,
+                ];
+            }
+            if ($adjustmentLineProduct->material()->exists()) {
+
+            }
+        }
+        if (count($stockMovementData)) {
+            StockMovement::insertMany($stockMovementData);
+        }
     }
 }
