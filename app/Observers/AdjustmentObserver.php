478fe73743f6d9b25c34e0f1b67d87d103c86126
Dlareg <johngeraldcayabyab@gmail.com>
2022-03-31 09:25:29 +0800

    updated variable name

--- app/Observers/AdjustmentObserver.php
+++ app/Observers/AdjustmentObserver.php
@@ -7,13 +7,13 @@ use App\Models\Sequence;
 
 class AdjustmentObserver
 {
-    public function created(Adjustment $model)
+    public function created(Adjustment $adjustment)
     {
-        $warehouse = $model->warehouse;
+        $warehouse = $adjustment->warehouse;
         $operationType = $warehouse->adjustmentOperationType;
         $adjustmentSequence = $operationType->referenceSequence;
         if ($adjustmentSequence) {
-            $model->number = Sequence::generateSequence($adjustmentSequence->id);
+            $adjustment->number = Sequence::generateSequence($adjustmentSequence->id);
             $adjustmentSequence->next_number = $adjustmentSequence->next_number + $adjustmentSequence->step;
             $adjustmentSequence->save();
         }
