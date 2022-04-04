8a1395e6c331c4d5f1151a5b5bd40cb12a3d6cd2
Gerald <johngeraldcayabyab@gmail.com>
2022-04-04 10:28:15 +0800

    refactoring custom menu

--- resources/js/Components/MenuLink.js
+++ resources/js/Components/MenuLink.js
@@ -4,8 +4,10 @@ import {Link} from "react-router-dom";
 
 const MenuLink = (props) => {
     return (
-        <Menu.Item key={props.key}>
-            <Link to={props.to}>{props.label}</Link>
+        <Menu.Item key={props.key} onClick={props.onClick}>
+            {props.to ?
+                <Link to={props.to}>{props.label}</Link>
+                : props.label}
         </Menu.Item>
     )
 };
