9ea39551a2f383fc7aaca0cdb9ca7eb327575773
Dlareg <johngeraldcayabyab@gmail.com>
2022-04-08 11:22:31 +0800

    transfer line edit now affects sales edit

--- app/Events/MatchTransferToSalesOrderLine.php
+++ app/Events/MatchTransferToSalesOrderLine.php
@@ -2,35 +2,79 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
+use App\Models\SalesOrderLine;
+use App\Models\SalesOrderTransferLine;
+use App\Models\Transfer;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MatchTransferToSalesOrderLine
+class MatchTransferToSalesOrderLine implements ShouldQueue
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function __construct(Transfer $transfer)
     {
-        //
+        if ($transfer->salesOrderTransfer()->exists()) {
+            $transferLines = $transfer->transferLines;
+            $salesOrderTransfer = $transfer->salesOrderTransfer;
+            $salesOrderLineData = [];
+            $newTransferLines = [];
+            $salesOrder = null;
+            foreach ($transferLines as $transferLine) {
+                if ($transferLine->salesOrderTransferLine()->exists()) {
+                    $salesOrder = $transferLine->salesOrderTransferLine->salesOrder;
+                    $salesOrderLine = $transferLine->salesOrderTransferLine->salesOrderLine;
+                    $salesOrderLine->description = $transferLine->description;
+                    $salesOrderLine->quantity = $transferLine->demand;
+                    $salesOrderLine->measurement_id = $transferLine->measurement_id;
+                    $salesOrderLine = $salesOrderLine->toArray();
+                    unset($salesOrderLine['updated_at']);
+                    $salesOrderLineData[] = $salesOrderLine;
+                } else {
+                    $salesOrderLineData[] = [
+                        'product_id' => $transferLine->product_id,
+                        'description' => $transferLine->description,
+                        'quantity' => $transferLine->demand,
+                        'measurement_id' => $transferLine->measurement_id,
+                        'created_at' => $transferLine->created_at,
+                    ];
+                    $newTransferLines[] = $transferLine;
+                }
+            }
+            if (count($salesOrderLineData)) {
+                SalesOrderLine::updateOrCreateMany($salesOrderLineData, $salesOrder->id);
+                $this->createSalesOrderTransferLines($salesOrderTransfer, $salesOrder, $transfer, $newTransferLines);
+            }
+        }
     }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
+    private function createSalesOrderTransferLines
+    (
+        $salesOrderTransfer,
+        $salesOrder,
+        $transfer,
+        $transferLines
+    )
     {
-        return new PrivateChannel('channel-name');
+        $lines = [];
+        foreach ($transferLines as $salesOrderLine) {
+            $salesOrderLine = SalesOrderLine::where('sales_order_id', $salesOrder->id)
+                ->where('product_id', $salesOrderLine->product_id)
+                ->where('measurement_id', $salesOrderLine->measurement_id)
+                ->where('created_at', $salesOrderLine->created_at)
+                ->first();
+            $lines[] = [
+                'sales_order_id' => $salesOrder->id,
+                'transfer_id' => $transfer->id,
+                'sales_order_line_id' => $salesOrderLine->id,
+                'transfer_line_id' => $salesOrderLine->id,
+                'sales_order_transfer_id' => $salesOrderTransfer->id,
+            ];
+        }
+        if (count($lines)) {
+            SalesOrderTransferLine::updateOrCreateMany($lines, $salesOrderTransfer->id);
+        }
     }
 }
