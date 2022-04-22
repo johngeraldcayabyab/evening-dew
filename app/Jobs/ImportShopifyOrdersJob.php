6d1c05a3ed0d4c4d5ac7dac76a30664177cf93b0
Dlareg <johngeraldcayabyab@gmail.com>
2022-04-22 16:54:46 +0800

    working

--- app/Jobs/ImportShopifyOrdersJob.php
+++ app/Jobs/ImportShopifyOrdersJob.php
@@ -2,6 +2,12 @@
 
 namespace App\Jobs;
 
+use App\Events\ContactUpsertEvent;
+use App\Models\Address;
+use App\Models\Contact;
+use App\Models\SalesOrder;
+use App\Models\Transfer;
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -20,21 +26,88 @@ class ImportShopifyOrdersJob implements ShouldQueue
 
     public function handle()
     {
+        $shopifyLink = env('SHOPIFY_URL');
+        $shopifyAccessToken = env('SHOPIFY_ACCESS_TOKEN');
+
         $response = Http::withHeaders([
             'Content-Type' => 'application/json',
-            'X-Shopify-Access-Token' => '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'
-        ])->get('https://tasteandtell.myshopify.com/admin/api/2022-04/orders.json?status=any');
+            'X-Shopify-Access-Token' => $shopifyAccessToken
+        ])->get("{$shopifyLink}/admin/api/2022-04/orders.json?status=any");
 
         $responseJson = $response->json();
         $orders = $responseJson['orders'];
 
 
         foreach ($orders as $order) {
-            $orderNumber = $responseJson['order_number'];
-            $email = $responseJson['email'];
+            $orderNumber = $order['order_number'];
+            $email = $order['email'];
+            $createdAt = Carbon::parse($order['created_at']);
+
+            $address = [];
+
+            if (isset($order['shipping_address'])) {
+                $address = $order['shipping_address'];
+            } else if (isset($order['billing_address'])) {
+                $address = $order['billing_address'];
+            }
+
+            $contact = Contact::where('name', $address['name'])->where('mobile', $address['phone'])->where('email', $email)->first();
+            if (!$contact) {
+                Contact::updateOrCreate([
+                    'name' => $address['name'],
+                    'mobile' => $address['phone'],
+                    'email' => $email,
+                ]);
+                $contact = Contact::where('name', $address['name'])->where('mobile', $address['phone'])->where('email', $email)->first();
+            }
+
+            $address = Address::where('address_name', $contact->name . " " . Address::DEFAULT . " address")
+                ->where('street_one', $address['address1'])
+                ->where('zip', $address['zip'])
+                ->where('city', $address['city'])
+                ->where('type', Address::DEFAULT)
+                ->where('country_id', 1)
+                ->where('contact_id', $contact->id)
+                ->first();
+            if (!$address) {
+                Address::updateOrCreate([
+                    'address_name' => $contact->name . " " . Address::DEFAULT . " address",
+                    'street_one' => $address['address1'],
+                    'zip' => $address['zip'],
+                    'city' => $address['city'],
+                    'type' => Address::DEFAULT,
+                    'country_id' => 1,
+                    'contact_id' => $contact->id,
+                ]);
+                $address = Address::where('address_name', $contact->name . " " . Address::DEFAULT . " address")
+                    ->where('street_one', $address['address1'])
+                    ->where('zip', $address['zip'])
+                    ->where('city', $address['city'])
+                    ->where('type', Address::DEFAULT)
+                    ->where('country_id', 1)
+                    ->where('contact_id', $contact->id)
+                    ->first();
+            }
+
+            if (!SalesOrder::where('number', $orderNumber)->first()) {
+                SalesOrder::updateOrCreate([
+                    'number' => $orderNumber,
+                    'customer_id' => $contact->id,
+                    'invoice_address_id' => $address->id,
+                    'delivery_address_id' => $address->id,
+                    'phone' => $address['phone'],
+                    'quotation_date' => now(),
+                    'salesperson_id' => 1,
+                    'shipping_policy' => Transfer::AS_SOON_AS_POSSIBLE,
+                    'source_document' => "Shopify {$orderNumber}",
+                    'status' => SalesOrder::DRAFT,
+                    'created_at' => $createdAt,
+                    'updated_at' => $createdAt,
+                ]);
+            }
         }
 
 
-        info($orders[0]);
+//        info($orders[0]);
     }
 }
