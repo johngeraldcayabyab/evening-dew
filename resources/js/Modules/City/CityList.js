7e6de1a45a7044f5d9b3d4b04b11ce3a6f2468c0
Gerald <johngeraldcayabyab@gmail.com>
2022-05-12 16:08:42 +0800

    added city front-end

---
+++ resources/js/Modules/City/CityList.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import useListHook from "../../Hooks/useListHook";
+import manifest from "./__manifest__.json";
+import TableCreateButton from "../../Components/TableButtons/TableCreateButton";
+import ControlPanel from "../../Components/ControlPanel";
+import CustomTable from "../../Components/CustomTable";
+import ActionsDropdownButton from "../../Components/TableButtons/ActionsDropdownButton";
+import CustomPagination from "../../Components/CustomPagination";
+import TableSearchInput from "../../Components/TableSearchInput";
+import CustomBreadcrumb from "../../Components/CustomBreadcrumb";
+import {ListContextProvider} from "../../Contexts/ListContext";
+
+const CityList = () => {
+    const [tableState, tableActions] = useListHook(manifest);
+    return (
+        <ListContextProvider value={{
+            manifest: manifest,
+            tableState: tableState,
+            tableActions: tableActions,
+            columns: [
+                {
+                    title: 'Name',
+                    dataIndex: 'name',
+                    key: 'name',
+                    sorter: true,
+                    searchFilter: true,
+                },
+                {
+                    title: 'Province',
+                    dataIndex: 'province',
+                    key: 'province',
+                    sorter: true,
+                    searchFilter: true,
+                },
+                {
+                    title: 'Region',
+                    dataIndex: 'region',
+                    key: 'region',
+                    sorter: true,
+                    searchFilter: true,
+                    render: (text, record) => {
+                        if (record.region) {
+                            return record.region.region;
+                        }
+                        return null;
+                    }
+                },
+                {
+                    title: 'Created At',
+                    dataIndex: 'created_at',
+                    key: 'created_at',
+                    sorter: true,
+                }
+            ]
+        }}>
+            <ControlPanel
+                topColOneLeft={<CustomBreadcrumb/>}
+                topColTwoRight={<TableSearchInput/>}
+                bottomColOneLeft={<TableCreateButton/>}
+                bottomColOneRight={<ActionsDropdownButton/>}
+                bottomColTwoRight={<CustomPagination/>}
+            />
+            <CustomTable/>
+        </ListContextProvider>
+    )
+};
+
+export default CityList;
