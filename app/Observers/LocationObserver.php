11b62a34c30dc99f41e2fb8beecb48ee2d657eca
Gerald <johngeraldcayabyab@gmail.com>
2022-05-13 14:50:02 +0800

    added defaults

--- app/Observers/LocationObserver.php
+++ app/Observers/LocationObserver.php
@@ -7,10 +7,24 @@ use Illuminate\Validation\ValidationException;
 
 class LocationObserver
 {
+    public function creating(Location $location)
+    {
+        $this->setDefaults($location);
+    }
+
     public function updating(Location $location)
     {
         if ($location->parent_location_id === $location->id) {
             throw ValidationException::withMessages(['parent_location_id' => 'Recursion detected']);
+        }
+        $this->setDefaults($location);
+    }
+
+    public function setDefaults($model)
+    {
+        $modelArray = $model->toArray();
+        if (!isset($modelArray['type'])) {
+            $model->type = Location::INTERNAL;
         }
     }
 }
