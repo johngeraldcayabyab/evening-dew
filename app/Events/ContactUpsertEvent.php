32091d64d05a3b0ba29bb65a1759135e274cf32d
Gerald <johngeraldcayabyab@gmail.com>
2022-05-17 10:15:09 +0800

    removed unused imports on event

--- app/Events/ContactUpsertEvent.php
+++ app/Events/ContactUpsertEvent.php
@@ -4,7 +4,6 @@ namespace App\Events;
 
 use App\Models\Contact;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -19,10 +18,5 @@ class ContactUpsertEvent
     {
         $this->contact = $contact;
         $this->data = $data;
-    }
-
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
     }
 }
