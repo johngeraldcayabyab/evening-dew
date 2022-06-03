e7278a9981f37266e9d83aa5968d1f27a007edc5
Dlareg <johngeraldcayabyab@gmail.com>
2022-06-03 09:29:54 +0800

    fix no keys, and pagination error

--- resources/js/Components/CustomPagination.js
+++ resources/js/Components/CustomPagination.js
@@ -1,18 +1,30 @@
-import {Pagination} from "antd";
+import {Pagination, Skeleton} from "antd";
 import {TableContext} from "../Contexts/TableContext";
 import {useContext} from "react";
 
 const CustomPagination = () => {
     const listContext = useContext(TableContext);
+
+    if (listContext.tableState.loading) {
+        return (
+            <Skeleton.Input
+                className={'ant-skeleton-element-custom'}
+                style={{display: 'block', width: '100%'}}
+                active={true}
+            />
+        );
+    }
+
     return (
         <Pagination
             size={'small'}
             current={listContext.tableState.meta ? listContext.tableState.meta.current_page : 1}
             total={listContext.tableState.meta ? listContext.tableState.meta.total : 1}
+            simple={true}
             pageSize={30} // not respecting meta condition if null
             showSizeChanger={false}
             showQuickJumper
-            simple={true}
+
             onChange={(page, pageSize) => {
                 listContext.tableState.params.page = page;
                 listContext.tableActions.renderData(listContext.tableState.params);
