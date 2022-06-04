b9dc931b7d9b827afea65d8e21c71fb448514bf5
Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2022-06-04 11:41:00 +0800

    added source object to initial values

--- app/Http/Controllers/SalesOrderController.php
+++ app/Http/Controllers/SalesOrderController.php
@@ -10,6 +10,7 @@ use App\Models\GlobalSetting;
 use App\Models\SalesOrder;
 use App\Models\SalesOrderLine;
 use App\Models\Sequence;
+use App\Models\Source;
 use App\Models\Transfer;
 use App\Traits\ControllerHelperTrait;
 use Illuminate\Http\JsonResponse;
@@ -93,6 +94,7 @@ class SalesOrderController
 
         if ($request->source_id) {
             $initialValues['source_id'] = $request->source_id;
+            $initialValues['source'] = Source::find($request->source_id);
         }
 
         return $initialValues;
