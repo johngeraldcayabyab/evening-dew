144f9a9888ee0a84f4932f2071086962685fa9c6
Dlareg <johngeraldcayabyab@gmail.com>
2022-06-04 13:28:25 +0800

    mapped additional data

--- app/Jobs/ImportShopifyOrdersJob.php
+++ app/Jobs/ImportShopifyOrdersJob.php
@@ -43,8 +43,8 @@ class ImportShopifyOrdersJob implements ShouldQueue
 
         foreach ($orders as $order) {
             $shopifyOrderNumber = 'SP/' . $order['order_number'];
-            if (SalesOrder::where('number', $shopifyOrderNumber)->first()) {
-                continue;
+            if(isset($order['shipping_lines'])){
+//                $this->log($order['shipping_lines']);
             }
             $shopifyCustomer = $order['customer'];
             $shopifyCreatedAt = Carbon::parse($order['created_at']);
@@ -123,17 +123,16 @@ class ImportShopifyOrdersJob implements ShouldQueue
 
             foreach ($shippingProperties as $shippingProperty){
                 if($shippingProperty['name'] === 'Delivery Date'){
-                   $expectedShippingDate = $shippingProperty['value'];
+                    $shippingProperty['value'] = explode('-',$shippingProperty['value']);
+                   $expectedShippingDate = Carbon::parse($shippingProperty['value'][2] . '-' . $shippingProperty['value'][0] . '-' . $shippingProperty['value'][1]);
                 }
                 if($shippingProperty['name'] === 'Delivery Time'){
-                    $expectedShippingDate = $shippingProperty['value'];
+//                    $expectedShippingDate = $shippingProperty['value'];
                 }
                 if($shippingProperty['name'] === 'Additional Comments'){
-                    $expectedShippingDate = $shippingProperty['value'];
+                    $notes = $shippingProperty['value'];
                 }
             }
-
-//            $shopifyLineItems
 
             $salesOrder = SalesOrder::create([
                 'number' => $shopifyOrderNumber,
@@ -143,7 +142,7 @@ class ImportShopifyOrdersJob implements ShouldQueue
                 'invoice_city_id' => $salesOrderInvoiceCity->id,
                 'delivery_city_id' => $salesOrderDeliveryCity->id,
                 'phone' => $contact->phone,
-                'quotation_date' => now(),
+                'quotation_date' => $shopifyCreatedAt,
                 'expected_shipping_date' => $expectedShippingDate,
                 'notes' => $notes,
                 'salesperson_id' => 1,
