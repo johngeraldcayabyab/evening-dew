10e1e618cd8255c42b73061eacabb73898703645
Dlareg <johngeraldcayabyab@gmail.com>
2022-06-06 08:38:47 +0800

    merge proper changes from other branch

--- resources/js/Components/NextPreviousRecord.js
+++ resources/js/Components/NextPreviousRecord.js
@@ -8,18 +8,18 @@ const NextPreviousRecord = () => {
     const formContext = useContext(FormContext);
     const nextRecord = formContext.formState.initialValues.next_record;
     const previousRecord = formContext.formState.initialValues.previous_record;
-    const moduleName = formContext.manifest.moduleName;
+    const displayName = formContext.manifest.displayName;
     return (
         <Radio.Group value={'small'}>
             <Button type={'link'} disabled={!Number.isInteger(nextRecord)}>
                 <Link
-                    to={`/${moduleName}/${Number.isInteger(nextRecord) ? nextRecord : null}`}>
+                    to={`/${displayName}/${Number.isInteger(nextRecord) ? nextRecord : null}`}>
                     <LeftOutlined/>
                 </Link>
             </Button>
             <Button type={'link'} disabled={!Number.isInteger(previousRecord)}>
                 <Link
-                    to={`/${moduleName}/${Number.isInteger(previousRecord) ? previousRecord : null}`}>
+                    to={`/${displayName}/${Number.isInteger(previousRecord) ? previousRecord : null}`}>
                     <RightOutlined/>
                 </Link>
             </Button>
