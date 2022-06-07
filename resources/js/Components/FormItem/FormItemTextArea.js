666acf1c5f709a9f7cbfb8c3aeab37dc0be7ae1c
Gerald <johngeraldcayabyab@gmail.com>
2022-06-07 12:49:08 +0800

    updated notes to use text area

---
+++ resources/js/Components/FormItem/FormItemTextArea.js
@@ -0,0 +1,22 @@
+import {Form, Input} from "antd";
+import React, {useContext} from "react";
+import CustomInputSkeleton from "../CustomInputSkeleton";
+import {formItemFieldProps} from "../../Helpers/form";
+import {FormContext} from "../../Contexts/FormContext";
+
+const {TextArea} = Input;
+
+const FormItemTextArea = (props) => {
+    const formContext = useContext(FormContext);
+    const [formItemProps, fieldProps] = formItemFieldProps(props);
+
+    return (
+        <Form.Item {...formItemProps}>
+            {formContext.formState.loading ? <CustomInputSkeleton {...props}/> :
+                <TextArea {...fieldProps}/>
+            }
+        </Form.Item>
+    )
+};
+
+export default FormItemTextArea;
