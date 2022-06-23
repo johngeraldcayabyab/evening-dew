2d8f1967eefbdb151bcf9e8cdc4f5aa4bc248b6d
Dlareg <johngeraldcayabyab@gmail.com>
2022-06-24 01:47:27 +0800

    added delivery fee in seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    public function run()
+    {
+        Product::create([
+            'name' => 'Delivery Fee'
+        ]);
+    }
+}
