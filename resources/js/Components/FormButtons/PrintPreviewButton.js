2a65a488cf9d42474b01f3a07dfb3f6445717f7f
Dlareg <johngeraldcayabyab@gmail.com>
2022-06-24 19:50:07 +0800

    finalize print format

--- resources/js/Components/FormButtons/PrintPreviewButton.js
+++ resources/js/Components/FormButtons/PrintPreviewButton.js
@@ -10,26 +10,27 @@ const ComponentToPrint = React.forwardRef((props, ref) => {
 
     const products = values.sales_order_lines ? values.sales_order_lines.map((salesOrderLine) => {
         return (
-            <h5>{salesOrderLine.quantity} {salesOrderLine.product.internal_reference} {salesOrderLine.product.name}</h5>);
+            <p>{salesOrderLine.quantity} x {salesOrderLine.product.internal_reference} - {salesOrderLine.product.name}</p>);
     }) : [];
 
-    return (<div style={{position: 'fixed', marginTop: '-1000%'}}>
-        <div ref={ref}>
-            <div style={{color: 'black'}}>
-                <hr style={{'borderTop': 'dotted 1px black'}}/>
-                <h3>{values.number}</h3>
-                <h5>{values.customer ? values.customer.name : ''}</h5>
-                <br/>
-                {products}
-                <br/>
-                <h5>Ready By: {values.ready_by}</h5>
+    return (
+        <div style={{position: 'fixed', marginTop: '-1000%'}}>
+            <div ref={ref}>
+                <div style={{color: 'black'}}>
+                    <hr style={{'borderTop': 'dotted 1px black'}}/>
+                    <p style={{fontSize: '20px'}}><b>{values.number}</b></p>
+                    <p>{values.customer ? values.customer.name : ''}</p>
+                    <br/>
+                    {products}
+                    <br/>
+                    <p>Ready By: {values.ready_by}</p>
 
-                <br/>
-                <hr style={{'borderTop': 'dotted 1px black'}}/>
+                    <br/>
+                    <hr style={{'borderTop': 'dotted 1px black'}}/>
+                </div>
             </div>
-
         </div>
-    </div>);
+    );
 });
 
 
