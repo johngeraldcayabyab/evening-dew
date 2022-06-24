729f69d97753f5b435dbebe3f241e765d77fb4a0
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2022-06-24 18:52:21 +0800

    print fix

--- resources/js/Components/FormButtons/PrintPreviewButton.js
+++ resources/js/Components/FormButtons/PrintPreviewButton.js
@@ -9,17 +9,38 @@ const ComponentToPrint = React.forwardRef((props, ref) => {
     const values = formContext.formState.initialValues;
 
     const products = values.sales_order_lines ? values.sales_order_lines.map((salesOrderLine) => {
-        return (
-            <tr>
-                <td className="quantity">{salesOrderLine.quantity}</td>
-                <td className="description">{salesOrderLine.product.internal_reference}</td>
-                <td className="price">{salesOrderLine.product.name}</td>
-            </tr>
-        )
+        return (<tr style={{
+            borderTop: '1px solid black',
+            borderCollapse: 'collapse',
+            fontSize: '12px',
+            fontFamily: 'Times New Roman'
+        }}>
+            <td style={{
+                borderTop: '1px solid black',
+                borderCollapse: 'collapse',
+                fontSize: '12px',
+                fontFamily: 'Times New Roman'
+            }}>{salesOrderLine.quantity}</td>
+            <td style={{
+                borderTop: '1px solid black',
+                borderCollapse: 'collapse',
+                fontSize: '12px',
+                fontFamily: 'Times New Roman'
+            }}>{salesOrderLine.product.internal_reference}</td>
+            <td style={{
+                borderTop: '1px solid black',
+                borderCollapse: 'collapse',
+                fontSize: '12px',
+                fontFamily: 'Times New Roman'
+            }}>{salesOrderLine.product.name}</td>
+        </tr>)
     }) : [];
 
     return (<div className={'print-preview'}>
-        <div className={'ticket'} ref={ref}>
+        <div style={{
+            width: '155px',
+            maxWidth: '155px',
+        }} ref={ref}>
 
             <p>
                 {values.number}
@@ -27,17 +48,28 @@ const ComponentToPrint = React.forwardRef((props, ref) => {
                 {values.customer ? values.customer.name : ''}
             </p>
 
-
-            <table>
-                <tbody>
+            <table style={{
+                border: '1px solid black',
+                borderCollapse: 'collapse',
+                fontSize: '12px',
+                fontFamily: 'Times New Roman'
+            }}>
+                <tbody style={{
+                    borderTop: '1px solid black',
+                    borderCollapse: 'collapse',
+                    fontSize: '12px',
+                    fontFamily: 'Times New Roman'
+                }}>
                 {products}
                 </tbody>
             </table>
 
-
-            <p className="centered">Ready By: {values.ready_by}</p>
-
-
+            <p style={{
+                textAlign: 'center',
+                alignContent: 'center',
+                fontSize: '12px',
+                fontFamily: 'Times New Roman'
+            }}>Ready By: {values.ready_by}</p>
         </div>
     </div>);
 });
