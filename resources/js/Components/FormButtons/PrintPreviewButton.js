89ea307e5f040a9b31bd189d7766e2cd4c91ba64
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2022-06-24 19:13:12 +0800

    test color print

--- resources/js/Components/FormButtons/PrintPreviewButton.js
+++ resources/js/Components/FormButtons/PrintPreviewButton.js
@@ -9,69 +9,38 @@ const ComponentToPrint = React.forwardRef((props, ref) => {
     const values = formContext.formState.initialValues;
 
     const products = values.sales_order_lines ? values.sales_order_lines.map((salesOrderLine) => {
-        return (<tr style={{
-            borderTop: '1px solid black',
-            borderCollapse: 'collapse',
-            fontSize: '12px',
-            fontFamily: 'Times New Roman'
-        }}>
-            <td style={{
-                borderTop: '1px solid black',
-                borderCollapse: 'collapse',
-                fontSize: '12px',
-                fontFamily: 'Times New Roman'
-            }}>{salesOrderLine.quantity}</td>
-            <td style={{
-                borderTop: '1px solid black',
-                borderCollapse: 'collapse',
-                fontSize: '12px',
-                fontFamily: 'Times New Roman'
-            }}>{salesOrderLine.product.internal_reference}</td>
-            <td style={{
-                borderTop: '1px solid black',
-                borderCollapse: 'collapse',
-                fontSize: '12px',
-                fontFamily: 'Times New Roman'
-            }}>{salesOrderLine.product.name}</td>
-        </tr>)
+        return (
+            <tr>
+                <td className="quantity">{salesOrderLine.quantity}</td>
+                <td className="description">{salesOrderLine.product.internal_reference}</td>
+                <td className="price">{salesOrderLine.product.name}</td>
+            </tr>
+        )
     }) : [];
 
-    return (<div className={'print-preview'}>
-        <div style={{
-            width: '155px',
-            maxWidth: '155px',
-        }} ref={ref}>
+    return (
+        <div ref={ref}>
+            <div className={'print-preview'}>
+                <div className={'ticket'}>
+                    <p>
+                        {values.number}
+                        <br/>
+                        {values.customer ? values.customer.name : ''}
+                    </p>
 
-            <p>
-                {values.number}
-                <br/>
-                {values.customer ? values.customer.name : ''}
-            </p>
 
-            <table style={{
-                border: '1px solid black',
-                borderCollapse: 'collapse',
-                fontSize: '12px',
-                fontFamily: 'Times New Roman'
-            }}>
-                <tbody style={{
-                    borderTop: '1px solid black',
-                    borderCollapse: 'collapse',
-                    fontSize: '12px',
-                    fontFamily: 'Times New Roman'
-                }}>
-                {products}
-                </tbody>
-            </table>
+                    <table>
+                        <tbody>
+                        {products}
+                        </tbody>
+                    </table>
 
-            <p style={{
-                textAlign: 'center',
-                alignContent: 'center',
-                fontSize: '12px',
-                fontFamily: 'Times New Roman'
-            }}>Ready By: {values.ready_by}</p>
+
+                    <p className="centered">Ready By: {values.ready_by}</p>
+                </div>
+            </div>
         </div>
-    </div>);
+    );
 });
 
 
