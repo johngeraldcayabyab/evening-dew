de6b600e89518bbbfa107ee7c39fba157bd3f4fd
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2022-07-22 17:30:58 +0800

    Added export sales order lines

--- app/Exports/SalesOrderExport.php
+++ app/Exports/SalesOrderExport.php
@@ -3,6 +3,8 @@
 namespace App\Exports;
 
 use App\Models\SalesOrder;
+use App\Models\SalesOrderLine;
+use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
@@ -13,15 +15,41 @@ class SalesOrderExport implements FromQuery, ShouldAutoSize, WithMapping, WithHe
 {
     use Exportable;
 
+    public function __construct($param)
+    {
+        $this->param = $param;
+    }
+
     public function query()
     {
-        return SalesOrder::query()->orderBy('created_at', 'desc');
+        $shippingFrom = null;
+        $shippingTo = null;
+        $isShippingDate = isset($this->param['shipping_date']);
+        if ($isShippingDate) {
+            $shippingDateRange = explode(',', $this->param['shipping_date']);
+            $shippingFrom = Carbon::parse($shippingDateRange[0]);
+            $shippingTo = Carbon::parse($shippingDateRange[1]);
+        }
+        $salesOrderQuery = SalesOrderLine::query();
+        if ($isShippingDate) {
+            $salesOrderQuery = $salesOrderQuery->whereHas('salesOrder', function ($query) use ($shippingFrom, $shippingTo) {
+                return $query->whereBetween('shipping_date', [$shippingFrom, $shippingTo]);
+            });
+        }
+        return $salesOrderQuery->orderBy('created_at', 'desc');
     }
 
     public function map($row): array
     {
         return [
-            'number' => $row->number,
+            'number' => $row->salesOrder->number,
+            'customer' => $row->salesOrder->customer->name,
+            'quantity' => $row->quantity,
+            'sku' => $row->product->internal_reference,
+            'name' => $row->product->name,
+            'delivery_address' => $row->salesOrder->delivery_address,
+            'delivery_city' => $row->salesOrder->deliveryCity ? $row->salesOrder->deliveryCity->name : '',
+            'delivery_phone' => $row->salesOrder->delivery_phone,
         ];
     }
 
@@ -29,6 +57,13 @@ class SalesOrderExport implements FromQuery, ShouldAutoSize, WithMapping, WithHe
     {
         return [
             'Number',
+            'Customer',
+            'Quantity',
+            'SKU',
+            'Name',
+            'Address',
+            'City',
+            'Contact'
         ];
     }
 }
