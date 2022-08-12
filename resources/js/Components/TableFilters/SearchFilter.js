bce31c1278141e8bc658a65f037226609edd55c7
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2022-08-12 12:12:38 +0800

    vertical space on search filter

--- resources/js/Components/TableFilters/SearchFilter.js
+++ resources/js/Components/TableFilters/SearchFilter.js
@@ -5,38 +5,40 @@ import {replaceUnderscoreWithSpace, titleCase} from "../../Helpers/string";
 const SearchFilter = (props) => {
     return (
         <div style={{padding: 8}}>
-            <Input
-                placeholder={`Search ${titleCase(replaceUnderscoreWithSpace(props.dataIndex))}`}
-                value={props.selectedKeys}
-                onChange={e => props.setSelectedKeys(e.target.value ? e.target.value : null)}
-                onPressEnter={(e) => {
-                    props.confirm();
-                    e.stopPropagation();
-                }}
-                // style={{marginBottom: 8, display: 'block'}}
-            />
-            <Space>
-                <Button
-                    type="primary"
-                    onClick={() => {
+            <Space direction={"vertical"}>
+                <Input
+                    placeholder={`Search ${titleCase(replaceUnderscoreWithSpace(props.dataIndex))}`}
+                    value={props.selectedKeys}
+                    onChange={e => props.setSelectedKeys(e.target.value ? e.target.value : null)}
+                    onPressEnter={(e) => {
                         props.confirm();
+                        e.stopPropagation();
                     }}
-                    icon={<SearchOutlined/>}
-                    size="small"
-                    style={{width: 90}}
-                >
-                    Search
-                </Button>
-                <Button
-                    onClick={() => {
-                        props.clearFilters();
-                        props.confirm();
-                    }}
-                    size={"small"}
-                    style={{width: 90}}
-                >
-                    Reset
-                </Button>
+                    // style={{marginBottom: 8, display: 'block'}}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => {
+                            props.confirm();
+                        }}
+                        icon={<SearchOutlined/>}
+                        size="small"
+                        style={{width: 90}}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => {
+                            props.clearFilters();
+                            props.confirm();
+                        }}
+                        size={"small"}
+                        style={{width: 90}}
+                    >
+                        Reset
+                    </Button>
+                </Space>
             </Space>
         </div>
     )
