a88fda0e1011a05e3207e7b3a323b703b6d37d58
Dlareg <johngeraldcayabyab@gmail.com>
2022-08-12 09:10:55 +0800

    get changes from taste

--- app/Http/Controllers/SalesOrderLineController.php
+++ app/Http/Controllers/SalesOrderLineController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Exports\SalesOrderLineExport;
+use App\Http\Resources\SalesOrderLineResource;
+use App\Models\SalesOrderLine;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
 class SalesOrderLineController
 {
+    public function index(Request $request)
+    {
+        $model = new SalesOrderLine();
+        $model = $model->filterAndOrder($request);
+        return SalesOrderLineResource::collection($model);
+    }
+
     public function export(Request $request)
     {
         return Excel::download(new SalesOrderLineExport($request->all()), 'sales_orders.xlsx');
