0120749944722438281dd9acd16e99f92a1d6e2d
Dlareg <johngeraldcayabyab@gmail.com>
2022-08-18 12:38:12 +0800

    Fix conflicting auto generated sequence

--- app/Observers/SalesOrderObserver.php
+++ app/Observers/SalesOrderObserver.php
@@ -14,19 +14,7 @@ class SalesOrderObserver
     public function creating(SalesOrder $model)
     {
         $this->setDefaults($model);
-    }
-
-    public function created(SalesOrder $salesOrder)
-    {
-        $salesOrderDefaultSequence = GlobalSetting::latestFirst()->salesOrderDefaultSequence;
-        if ($salesOrderDefaultSequence) {
-
-            if($salesOrder->number === Sequence::generateSalesOrderSequence()){
-                $salesOrderDefaultSequence->next_number = $salesOrderDefaultSequence->next_number + $salesOrderDefaultSequence->step;
-                $salesOrderDefaultSequence->save();
-            }
-
-        }
+        $this->setOrder($model);
     }
 
     public function updating(SalesOrder $model)
@@ -45,7 +33,7 @@ class SalesOrderObserver
             $model->expiration_date = Carbon::parse($model->expiration_date)->format(SystemSetting::DATE_TIME_FORMAT);
         }
 
-        if($model->quotation_date){
+        if ($model->quotation_date) {
             $model->quotation_date = Carbon::parse($model->quotation_date)->format(SystemSetting::DATE_TIME_FORMAT);
         }
 
@@ -62,6 +50,24 @@ class SalesOrderObserver
         }
         if (!$model->shipping_method) {
             $model->shipping_method = Transfer::DELIVERY;
+        }
+    }
+
+    public function setOrder($salesOrder)
+    {
+        $number = $salesOrder->number;
+        $salesOrderDefaultSequence = GlobalSetting::latestFirst()->salesOrderDefaultSequence;
+        if ($salesOrderDefaultSequence) {
+            $prefix = preg_replace('/([^A-Za-z0-9\s])/', '\\\\$1', $salesOrderDefaultSequence->prefix);
+            $steps = $salesOrderDefaultSequence->sequence_size;
+            $suffix = preg_replace('/([^A-Za-z0-9\s])/', '\\\\$1', $salesOrderDefaultSequence->suffix);
+            preg_match("/$prefix\d{" . $steps . "}$suffix$/", $number, $matches);
+            if (count($matches)) {
+                $salesOrder->number = Sequence::generateSalesOrderSequence();
+                $salesOrderDefaultSequenceNew = GlobalSetting::latestFirst()->salesOrderDefaultSequence;
+                $salesOrderDefaultSequenceNew->next_number = $salesOrderDefaultSequence->next_number + $salesOrderDefaultSequence->step;
+                $salesOrderDefaultSequenceNew->save();
+            }
         }
     }
 }
