db85d5e24b58c1724167fef0d742a8a525dc33f8
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2022-10-04 09:27:38 +0800

    encapsulated field getter

--- app/Traits/ResourceHelper.php
+++ app/Traits/ResourceHelper.php
@@ -2,36 +2,41 @@
 
 namespace App\Traits;
 
-use App\Data\SystemSetting;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 trait ResourceHelper
 {
-    public function defaults($object, $request, $fields)
+    public function defaults($object, $request, $resource)
     {
         $timeStampFormat = 'm/d/Y h:i:s A';
-        $defaultFields = $fields;
+        $selectedResource = $this->getSelectedFieldsOnly($request, $resource);
+        $selectedResource['id'] = $object->id;
+        if ($object->created_at) {
+            $selectedResource['created_at'] = $object->created_at->format($timeStampFormat);
+        }
+        if ($object->updated_at) {
+            $selectedResource['updated_at'] = $object->updated_at->format($timeStampFormat);
+        }
+        if ($object->deleted_at) {
+            $selectedResource['deleted_at'] = $object->deleted_at->format($timeStampFormat);
+        }
+        if ($object->id) {
+            $selectedResource['next_record'] = $object->nextRecord($object->id);
+            $selectedResource['previous_record'] = $object->previousRecord($object->id);
+        }
+        return $selectedResource;
+    }
+
+    private function getSelectedFieldsOnly($request, $resource)
+    {
+        $selectedResourceFields = $resource;
         if ($request->selected_fields) {
             if (Str::contains($request->url(), $this->getTable())) {
                 $selectedFields = explode(',', $request->selected_fields);
-                $defaultFields = Arr::only($defaultFields, $selectedFields);
+                $selectedResourceFields = Arr::only($selectedResourceFields, $selectedFields);
             }
         }
-        $defaultFields['id'] = $object->id;
-        if ($object->created_at) {
-            $defaultFields['created_at'] = $object->created_at->format($timeStampFormat);
-        }
-        if ($object->updated_at) {
-            $defaultFields['updated_at'] = $object->updated_at->format($timeStampFormat);
-        }
-        if ($object->deleted_at) {
-            $defaultFields['deleted_at'] = $object->deleted_at->format($timeStampFormat);
-        }
-        if ($object->id) {
-            $defaultFields['next_record'] = $object->nextRecord($object->id);
-            $defaultFields['previous_record'] = $object->previousRecord($object->id);
-        }
-        return $defaultFields;
+        return $selectedResourceFields;
     }
 }
