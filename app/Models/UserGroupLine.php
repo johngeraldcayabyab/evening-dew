f5890e06b77a1cba92cb313e9eeeb3fc5da0c1f2
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2022-11-08 15:59:24 +0800

    create user group lines

---
+++ app/Models/UserGroupLine.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\FilterTrait;
+use App\Traits\ModelHelperTrait;
+use App\Traits\NextAndPreviousRecordTrait;
+use Illuminate\Database\Eloquent\BroadcastsEvents;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class UserGroupLine extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    use BroadcastsEvents;
+    use FilterTrait;
+    use LogsActivity;
+    use ModelHelperTrait;
+    use NextAndPreviousRecordTrait;
+
+    protected $table = 'user_group_lines';
+    protected $guarded = [];
+    protected static $logAttributes = ['*'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function slug()
+    {
+        return 'name';
+    }
+
+    public function scopeMassUpsert($query, $data, $parent)
+    {
+        $lines = [];
+        $date = now();
+        foreach ($data as $datum) {
+            $line = [
+                'id' => isset($datum['id']) ? $datum['id'] : null,
+                'user_id' => $parent->id,
+                'updated_at' => $datum['updated_at'] ?? $date,
+            ];
+            if (!isset($datum['id'])) {
+                $line['created_at'] = $datum['created_at'] ?? $date;
+            }
+            $lines[] = $line;
+        }
+        $query->upsert($lines, ['id']);
+        return $query;
+    }
+}
---
+++ database/migrations/2022_11_08_154416_create_user_group_lines_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserGroupLinesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('user_group_lines', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->nullable();
+            $table->bigInteger('group_id')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('user_group_lines');
+    }
+}
