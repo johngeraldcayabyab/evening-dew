1da08250fa92c36e5576c0bd347bf6b86a75e1f4
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-02-03 18:31:01 +0800

    country now generated

--- resources/js/Modules/Country/CountryForm.js
+++ resources/js/Modules/Country/CountryForm.js
@@ -1,82 +1,9 @@
-import React, {useEffect} from 'react';
-import {Form} from "antd";
-import {useParams} from "react-router-dom";
-import useFormHook from "../../Hooks/useFormHook";
-import FormButtons from "../../Components/FormButtons/FormButtons";
-import RowForm from "../../Components/Grid/RowForm";
-import ColForm from "../../Components/Grid/ColForm";
-import CustomForm from "../../Components/CustomForm";
-import FormItemText from "../../Components/FormItem/FormItemText";
-import ControlPanel from "../../Components/ControlPanel";
-import FormCard from "../../Components/FormCard";
-import CustomBreadcrumb from "../../Components/CustomBreadcrumb";
-import {FormContextProvider} from "../../Contexts/FormContext";
-import FormItemSelect from "../../Components/FormItem/FormItemSelect";
-import useOptionHook from "../../Hooks/useOptionHook";
-import NextPreviousRecord from "../../Components/NextPreviousRecord";
+import React from 'react';
 import CountryManifest from "./CountryManifest"
+import FormGenerator from "../../Components/Form/FormGenerator"
 
 const CountryForm = () => {
-    let {id} = useParams();
-    const [form] = Form.useForm();
-    const [formState, formActions] = useFormHook(id, form, CountryManifest);
-    const currencyOptions = useOptionHook('/api/currencies', 'currency.currency');
-
-    useEffect(() => {
-        currencyOptions.getInitialOptions(formState);
-    }, [formState.initialLoad]);
-
-    return (
-        <FormContextProvider
-            value={{
-                id: id,
-                manifest: CountryManifest,
-                form: form,
-                formState: formState,
-                formActions: formActions,
-                onFinish: formActions.onFinish
-            }}
-        >
-            <CustomForm>
-                <ControlPanel
-                    topColOneLeft={<CustomBreadcrumb/>}
-                    bottomColOneLeft={<FormButtons/>}
-                    bottomColTwoRight={<NextPreviousRecord/>}
-                />
-                <FormCard>
-                    <RowForm>
-                        <ColForm>
-                            <FormItemText
-                                label={'Country Name'}
-                                name={'country_name'}
-                                message={'Please input country name'}
-                                required={true}
-                            />
-
-                            <FormItemSelect
-                                label={'Currency'}
-                                name={'currency_id'}
-                                {...currencyOptions}
-                            />
-
-                            <FormItemText
-                                label={'Country Code'}
-                                name={'country_code'}
-                            />
-                        </ColForm>
-
-                        <ColForm>
-                            <FormItemText
-                                label={'Country Calling Code'}
-                                name={'country_calling_code'}
-                            />
-                        </ColForm>
-                    </RowForm>
-                </FormCard>
-            </CustomForm>
-        </FormContextProvider>
-    );
+    return <FormGenerator {...CountryManifest}/>;
 };
-
 
 export default CountryForm;
--- resources/js/Modules/Country/CountryManifest.js
+++ resources/js/Modules/Country/CountryManifest.js
@@ -11,5 +11,36 @@ export default {
         {path: `/${displayName}/create`, component: CountryForm},
         {path: `/${displayName}/:id`, component: CountryForm},
         {path: `/${displayName}`, component: CountryTable},
-    ]
+    ],
+    form: {
+        row_1: {
+            col_1: [
+                {
+                    type: 'text',
+                    name: 'country_name',
+                    label: 'Country Name',
+                    message: 'Please input country name',
+                    required: true,
+                },
+                {
+                    type: 'select',
+                    name: 'currency_id',
+                    label: 'Currency',
+                    query: {url: '/api/currencies', field: 'currency.currency', name: 'currencyOptions'},
+                },
+                {
+                    type: 'text',
+                    name: 'country_code',
+                    label: 'Country Code',
+                },
+            ],
+            col_2: [
+                {
+                    type: 'text',
+                    name: 'country_calling_code',
+                    label: 'Country Calling Code',
+                },
+            ]
+        }
+    }
 };
