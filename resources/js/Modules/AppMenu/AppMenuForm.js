a71cfc8c997163dc41ab33329e5a5376f22d8bbf
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-02-03 17:56:55 +0800

    app menu refactored

--- resources/js/Modules/AppMenu/AppMenuForm.js
+++ resources/js/Modules/AppMenu/AppMenuForm.js
@@ -1,75 +1,9 @@
-import React, {useEffect} from 'react';
-import {Form} from "antd";
-import {useParams} from "react-router-dom";
-import useFormHook from "../../Hooks/useFormHook";
-import FormButtons from "../../Components/FormButtons/FormButtons";
-import RowForm from "../../Components/Grid/RowForm";
-import ColForm from "../../Components/Grid/ColForm";
-import CustomForm from "../../Components/CustomForm";
-import FormItemText from "../../Components/FormItem/FormItemText";
-import ControlPanel from "../../Components/ControlPanel";
-import FormCard from "../../Components/FormCard";
-import CustomBreadcrumb from "../../Components/CustomBreadcrumb";
-import {FormContextProvider} from "../../Contexts/FormContext";
-import useOptionHook from "../../Hooks/useOptionHook";
-import FormItemSelect from "../../Components/FormItem/FormItemSelect";
-import NextPreviousRecord from "../../Components/NextPreviousRecord";
+import React from 'react';
 import AppMenuManifest from "./AppMenuManifest"
+import FormGenerator from "../../Components/Form/FormGenerator"
 
 const AppMenuForm = () => {
-    let {id} = useParams();
-    const [form] = Form.useForm();
-    const [formState, formActions] = useFormHook(id, form, AppMenuManifest, true);
-    const menuOptions = useOptionHook('/api/menus', 'menu.label');
-    const appMenuOptions = useOptionHook('/api/app_menus', 'parent_app_menu.label');
-
-    useEffect(() => {
-        menuOptions.getInitialOptions(formState);
-        appMenuOptions.getInitialOptions(formState);
-    }, [formState.initialLoad]);
-
-    return (
-        <FormContextProvider
-            value={{
-                id: id,
-                manifest: AppMenuManifest,
-                form: form,
-                formState: formState,
-                formActions: formActions,
-                onFinish: formActions.onFinish
-            }}
-        >
-            <CustomForm>
-                <ControlPanel
-                    topColOneLeft={<CustomBreadcrumb/>}
-                    bottomColOneLeft={<FormButtons/>}
-                    bottomColTwoRight={<NextPreviousRecord/>}
-                />
-                <FormCard>
-                    <RowForm>
-                        <ColForm>
-                            <FormItemText
-                                label={'Label'}
-                                name={'label'}
-                                message={'Please input label'}
-                                required={true}
-                            />
-                            <FormItemSelect
-                                label={'Menu'}
-                                name={'menu_id'}
-                                {...menuOptions}
-                            />
-                            <FormItemSelect
-                                label={'Parent App Menu'}
-                                name={'parent_app_menu_id'}
-                                {...appMenuOptions}
-                            />
-                        </ColForm>
-                    </RowForm>
-                </FormCard>
-            </CustomForm>
-        </FormContextProvider>
-    );
+    return (<FormGenerator {...AppMenuManifest} />)
 };
 
 export default AppMenuForm;
--- resources/js/Modules/AppMenu/AppMenuManifest.js
+++ resources/js/Modules/AppMenu/AppMenuManifest.js
@@ -11,5 +11,30 @@ export default {
         {path: `/${displayName}/create`, component: AppMenuForm},
         {path: `/${displayName}/:id`, component: AppMenuForm},
         {path: `/${displayName}`, component: AppMenuTable},
-    ]
+    ],
+    form: {
+        row_1: {
+            col_1: [
+                {
+                    type: 'text',
+                    name: 'label',
+                    label: 'Label',
+                    message: 'Please input label',
+                    required: true,
+                },
+                {
+                    type: 'select',
+                    name: 'menu_id',
+                    label: 'Menu',
+                    query: {url: '/api/menus', field: 'menu.label', name: 'menuOptions'},
+                },
+                {
+                    type: 'select',
+                    name: 'parent_app_menu_id',
+                    label: 'Parent App Menu',
+                    query: {url: '/api/app_menus', field: 'parent_app_menu.label', name: 'appMenuOptions'},
+                },
+            ]
+        }
+    }
 };
