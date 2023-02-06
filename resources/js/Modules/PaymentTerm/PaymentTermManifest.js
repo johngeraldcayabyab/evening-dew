1aff7664d4fbbba1798c05b5f81c8e7ae0a2fc80
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-02-06 16:54:37 +0800

    payment term form now generated

--- resources/js/Modules/PaymentTerm/PaymentTermManifest.js
+++ resources/js/Modules/PaymentTerm/PaymentTermManifest.js
@@ -3,6 +3,8 @@ import PaymentTermTable from "./PaymentTermTable";
 
 const displayName = "payment_terms";
 
+
+
 export default {
     "moduleName": "payment_terms",
     "displayName": displayName,
@@ -11,5 +13,18 @@ export default {
         {path: `/${displayName}/create`, component: PaymentTermForm},
         {path: `/${displayName}/:id`, component: PaymentTermForm},
         {path: `/${displayName}`, component: PaymentTermTable},
-    ]
+    ],
+    form: {
+        row_1: {
+            col_1: [
+                {
+                    type: 'text',
+                    name: 'name',
+                    label: 'Name',
+                    message: 'Please input name',
+                    required: true,
+                },
+            ]
+        }
+    }
 };
