d39563aa538b90ee95aa2dfce4cdfea3c781ffd5
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-02-06 16:47:00 +0800

    Stock movement now form generated

--- resources/js/Modules/StockMovement/StockMovementManifest.js
+++ resources/js/Modules/StockMovement/StockMovementManifest.js
@@ -11,5 +11,58 @@ export default {
         {path: `/${displayName}/create`, component: StockMovementForm},
         {path: `/${displayName}/:id`, component: StockMovementForm},
         {path: `/${displayName}`, component: StockMovementTable},
-    ]
+    ],
+    form: {
+        row_1: {
+            col_1: [
+                {
+                    type: 'text',
+                    name: 'reference',
+                    label: 'Reference',
+                    message: 'Please input reference',
+                    required: true,
+                },
+                {
+                    type: 'text',
+                    name: 'source',
+                    label: 'Source',
+                },
+                {
+                    type: 'select',
+                    name: 'product_id',
+                    label: 'Product',
+                    query: {url: '/api/products', field: 'product.name', name: 'productOptions'},
+                    message: 'Please select a product',
+                    required: true
+                },
+                {
+                    type: 'select',
+                    name: 'source_location_id',
+                    label: 'Please select source location',
+                    query: {url: '/api/locations', field: 'source_location.name', name: 'sourceLocationOptions'},
+                    message: 'Please select a source location',
+                    required: true
+                },
+                {
+                    type: 'select',
+                    name: 'destination_location_id',
+                    label: 'Please select destination location',
+                    query: {
+                        url: '/api/locations',
+                        field: 'destination_location.name',
+                        name: 'destinationLocationOptions'
+                    },
+                    message: 'Please select a destination location',
+                    required: true
+                },
+                {
+                    type: 'number',
+                    name: 'quantity_done',
+                    label: 'Quantity',
+                    message: 'Please input quantity done',
+                    required: true
+                },
+            ]
+        }
+    }
 };
