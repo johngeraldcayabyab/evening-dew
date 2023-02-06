2e37f1aef063843fc3c8b95b0fc1bd705d3a5650
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-02-07 00:26:43 +0800

    Measurement form now form generated

--- resources/js/Modules/Measurement/MeasurementManifest.js
+++ resources/js/Modules/Measurement/MeasurementManifest.js
@@ -11,5 +11,48 @@ export default {
         {path: `/${displayName}/create`, component: MeasurementForm},
         {path: `/${displayName}/:id`, component: MeasurementForm},
         {path: `/${displayName}`, component: MeasurementTable},
-    ]
+    ],
+    form: {
+        initialValue: true,
+        row_1: {
+            col_1: [
+                {
+                    type: 'text',
+                    name: 'name',
+                    label: 'Name',
+                    required: true,
+                },
+                {
+                    type: 'select',
+                    name: 'type',
+                    label: 'Please select a type',
+                    options: [
+                        {value: 'reference', label: 'Reference measurement for this category'},
+                        {value: 'smaller', label: 'Smaller than the reference measurement'},
+                        {value: 'bigger', label: 'Bigger than the reference measurement'},
+                    ],
+                    required: true,
+                },
+                {
+                    type: 'number',
+                    name: 'ratio',
+                    label: 'Ratio',
+                    required: true,
+                },
+                {
+                    type: 'number',
+                    name: 'rounding_precision',
+                    label: 'Rounding precision',
+                    required: true,
+                },
+                {
+                    type: 'select',
+                    name: 'measurement_category_id',
+                    label: 'Measurement Category',
+                    query: {url: '/api/measurement_categories', field: 'measurement_category.name'},
+                    required: true,
+                },
+            ]
+        }
+    }
 };
