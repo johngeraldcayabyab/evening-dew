d59f9be879c3f4e880646ccae9204ee2cb6db888
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-02-06 16:49:13 +0800

    region now form generated

--- resources/js/Modules/Region/RegionForm.js
+++ resources/js/Modules/Region/RegionForm.js
@@ -1,66 +1,9 @@
 import React from 'react';
-import {Form} from "antd";
-import {useParams} from "react-router-dom";
-import useFormHook from "../../Hooks/useFormHook";
-import FormButtons from "../../Components/FormButtons/FormButtons";
-import RowForm from "../../Components/Grid/RowForm";
-import ColForm from "../../Components/Grid/ColForm";
-import CustomForm from "../../Components/CustomForm";
-import FormItemText from "../../Components/FormItem/FormItemText";
-import ControlPanel from "../../Components/ControlPanel";
-import FormCard from "../../Components/FormCard";
-import CustomBreadcrumb from "../../Components/CustomBreadcrumb";
-import {FormContextProvider} from "../../Contexts/FormContext";
-import NextPreviousRecord from "../../Components/NextPreviousRecord";
 import RegionManifest from "./RegionManifest"
+import FormGenerator from "../../Components/Form/FormGenerator"
 
 const RegionForm = () => {
-    let {id} = useParams();
-    const [form] = Form.useForm();
-    const [formState, formActions] = useFormHook(id, form, RegionManifest);
-
-    return (
-        <FormContextProvider
-            value={{
-                id: id,
-                manifest: RegionManifest,
-                form: form,
-                formState: formState,
-                formActions: formActions,
-                onFinish: formActions.onFinish
-            }}
-        >
-            <CustomForm>
-                <ControlPanel
-                    topColOneLeft={<CustomBreadcrumb/>}
-                    bottomColOneLeft={<FormButtons/>}
-                    bottomColTwoRight={<NextPreviousRecord/>}
-                />
-                <FormCard>
-                    <RowForm>
-                        <ColForm>
-                            <FormItemText
-                                label={'Region'}
-                                name={'region'}
-                                message={'Please input region'}
-                                required={true}
-                                size={'large'}
-                            />
-
-                            <FormItemText
-                                label={'Region Center'}
-                                name={'region_center'}
-                                message={'Please input region center'}
-                                required={true}
-                                size={'large'}
-                            />
-                        </ColForm>
-                    </RowForm>
-                </FormCard>
-            </CustomForm>
-        </FormContextProvider>
-    );
+    return <FormGenerator {...RegionManifest} />;
 };
-
 
 export default RegionForm;
--- resources/js/Modules/Region/RegionManifest.js
+++ resources/js/Modules/Region/RegionManifest.js
@@ -11,5 +11,27 @@ export default {
         {path: `/${displayName}/create`, component: RegionForm},
         {path: `/${displayName}/:id`, component: RegionForm},
         {path: `/${displayName}`, component: RegionTable},
-    ]
+    ],
+    form: {
+        row_1: {
+            col_1: [
+                {
+                    type: 'text',
+                    name: 'region',
+                    label: 'Region',
+                    message: 'Please input region',
+                    required: true,
+                    size:'large'
+                },
+                {
+                    type: 'text',
+                    name: 'region_center',
+                    label: 'Region Center',
+                    message: 'Please input region center',
+                    required: true,
+                    size:'large'
+                },
+            ]
+        }
+    }
 };
