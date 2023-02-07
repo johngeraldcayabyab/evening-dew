4cb42b6880bce193633556f8bf8e053afe205aed
Dlareg <johngeraldcayabyab@gmail.com>
2023-02-07 18:14:44 +0800

    added sm north sales

--- resources/js/Modules/Home/Home.js
+++ resources/js/Modules/Home/Home.js
@@ -32,6 +32,12 @@ const Home = () => {
         manualSalesTo: moment(),
         manualPicker: 'month',
 
+        smNorthSalesLoading: true,
+        smNorthSales: [],
+        smNorthSalesFrom: moment().subtract(7, 'months'),
+        smNorthSalesTo: moment(),
+        smNorthPicker: 'month',
+
         // allSalesLoading: true,
         // allSales: [],
         // allSalesFrom: moment().subtract(7, 'months'),
@@ -44,8 +50,32 @@ const Home = () => {
         const to = moment();
         fetchShopifySales(from, to);
         fetchManualSales(from, to);
+        fetchSmNorthSales(from, to);
         // fetchAllSales(from, to);
     }, []);
+
+    function fetchSmNorthSales(from, to, dateUnit = false) {
+        useFetch(`/api/sales_orders/sales_per_day`, GET, {
+            from: from.format(dateFormat),
+            to: to.format(dateFormat),
+            source_id: 6,
+            date_unit: dateUnit ? dateUnit : state.smNorthPicker
+        }).then((response) => {
+            const sales = response.map((sales) => ({
+                ...sales,
+                total: parseInt(sales.total)
+            }));
+            setState((prevState) => ({
+                ...prevState,
+                smNorthSales: sales,
+                smNorthSalesLoading: false,
+                smNorthSalesFrom: from,
+                smNorthSalesTo: to
+            }));
+        }).catch((responseErr) => {
+            fetchCatcher.get(responseErr);
+        });
+    }
 
     function fetchShopifySales(from, to, dateUnit = false) {
         useFetch(`/api/sales_orders/sales_per_day`, GET, {
@@ -220,6 +250,7 @@ const Home = () => {
         fetchShopifySales(from, to, value);
     }
 
+
     function setManualPicker(value) {
         let dateUnit;
         let dateAmount;
@@ -243,6 +274,31 @@ const Home = () => {
         }));
 
         fetchManualSales(from, to, value);
+    }
+
+    function setSmNorthPicker(value) {
+        let dateUnit;
+        let dateAmount;
+        if (value === 'date') {
+            dateUnit = 'days';
+            dateAmount = 7;
+        } else if (value === 'month') {
+            dateUnit = 'months';
+            dateAmount = 7;
+        } else if (value === 'year') {
+            dateUnit = 'years';
+            dateAmount = 3;
+        }
+
+        const from = moment().subtract(dateAmount, dateUnit);
+        const to = moment();
+        setState((prevState) => ({
+            ...prevState,
+            smNorthPicker: value,
+            smNorthSalesFrom: from,
+        }));
+
+        fetchSmNorthSales(from, to, value);
     }
 
     return (<>
@@ -396,6 +452,149 @@ const Home = () => {
             </Col>
         </Row>
 
+
+        <Row align={'middle'} style={{marginTop: '15px'}}>
+            <Col
+                xs={{span: 24}}
+                sm={{span: 24}}
+                md={{span: 24}}
+                lg={{span: 12}}
+            >
+                <Card
+                    title={"SM North"}
+                    extra={
+                        <RangePicker
+                            onChange={e => {
+                                fetchSmNorthSales(moment(e[0]), moment(e[1]));
+                            }}
+                            allowClear={false}
+                            style={{marginBottom: 8, width: '100%'}}
+                            defaultValue={[state.smNorthSalesFrom, state.smNorthSalesTo]}
+                            picker={state.smNorthPicker}
+                        />
+                    }
+                    style={{margin: '5%', padding: '15px'}}
+                >
+                    <Radio.Group
+                        value={state.smNorthPicker}
+                        onChange={e => {
+                            setSmNorthPicker(e.target.value);
+                        }}
+                    >
+                        <Radio.Button value="date">Day</Radio.Button>
+                        <Radio.Button value="month">Month</Radio.Button>
+                        <Radio.Button value="year">Year</Radio.Button>
+                    </Radio.Group>
+
+
+                    <Table
+                        loading={state.smNorthSalesLoading}
+                        dataSource={state.smNorthSales}
+                        columns={[
+                            {
+                                title: 'Date',
+                                dataIndex: 'year',
+                                key: 'year',
+                                render: (text, record) => {
+                                    if (record.month) {
+                                        const month = String(record.month).length === 1 ? `0${record.month}` : record.month;
+                                        return `${record.year}-${month}`;
+                                    }
+                                    return record.year;
+                                }
+                            },
+                            {
+                                title: 'Total',
+                                dataIndex: 'total',
+                                key: 'total',
+                                render: (text, record) => {
+                                    return numberWithCommas(record.total)
+                                }
+                            }
+                            ,
+                        ]}
+                        rowKey={'total'}
+                        pagination={false}
+                        size={'small'}
+                    />
+
+                    {/*<Spin spinning={state.shopifySalesLoading}>*/}
+                    {/*    <Column {...shopifySalesConfig} />*/}
+                    {/*</Spin>*/}
+                </Card>
+            </Col>
+
+            {/*<Col*/}
+            {/*    xs={{span: 24}}*/}
+            {/*    sm={{span: 24}}*/}
+            {/*    md={{span: 24}}*/}
+            {/*    lg={{span: 12}}*/}
+            {/*>*/}
+            {/*    <Card*/}
+            {/*        title={"Manual Sales"}*/}
+            {/*        extra={*/}
+            {/*            <RangePicker*/}
+            {/*                onChange={e => {*/}
+            {/*                    fetchManualSales(moment(e[0]), moment(e[1]));*/}
+            {/*                }}*/}
+            {/*                allowClear={false}*/}
+            {/*                style={{marginBottom: 8, width: '100%'}}*/}
+            {/*                defaultValue={[state.manualSalesFrom, state.manualSalesTo]}*/}
+            {/*                picker={state.manualPicker}*/}
+            {/*            />*/}
+            {/*        }*/}
+            {/*        style={{margin: '5%', padding: '15px'}}*/}
+            {/*    >*/}
+            {/*        <Radio.Group*/}
+            {/*            value={state.manualPicker}*/}
+            {/*            onChange={e => {*/}
+            {/*                setManualPicker(e.target.value);*/}
+            {/*            }}*/}
+            {/*        >*/}
+            {/*            <Radio.Button value="date">Day</Radio.Button>*/}
+            {/*            <Radio.Button value="month">Month</Radio.Button>*/}
+            {/*            <Radio.Button value="year">Year</Radio.Button>*/}
+            {/*        </Radio.Group>*/}
+
+
+            {/*        <Table*/}
+            {/*            loading={state.manualSalesLoading}*/}
+            {/*            dataSource={state.manualSales}*/}
+            {/*            columns={[*/}
+            {/*                {*/}
+            {/*                    title: 'Date',*/}
+            {/*                    dataIndex: 'year',*/}
+            {/*                    key: 'year',*/}
+            {/*                    render: (text, record) => {*/}
+            {/*                        if (record.month) {*/}
+            {/*                            const month = String(record.month).length === 1 ? `0${record.month}` : record.month;*/}
+            {/*                            return `${record.year}-${month}`;*/}
+            {/*                        }*/}
+            {/*                        return record.year;*/}
+            {/*                    }*/}
+            {/*                },*/}
+            {/*                {*/}
+            {/*                    title: 'Total',*/}
+            {/*                    dataIndex: 'total',*/}
+            {/*                    key: 'total',*/}
+            {/*                    render: (text, record) => {*/}
+            {/*                        return numberWithCommas(record.total)*/}
+            {/*                    }*/}
+            {/*                }*/}
+            {/*                ,*/}
+            {/*            ]}*/}
+            {/*            rowKey={'total'}*/}
+            {/*            pagination={false}*/}
+            {/*            size={'small'}*/}
+            {/*        />*/}
+
+
+            {/*        /!*<Spin spinning={state.manualSalesLoading}>*!/*/}
+            {/*        /!*    <Column {...manualSalesConfig} />*!/*/}
+            {/*        /!*</Spin>*!/*/}
+            {/*    </Card>*/}
+            {/*</Col>*/}
+        </Row>
 
         {/*<Row align={'middle'} style={{marginTop: '15px'}}>*/}
         {/*    <Col*/}
