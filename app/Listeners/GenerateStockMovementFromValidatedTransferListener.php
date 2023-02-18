2aebbae0e9e3d5dd62769e75a227be0468a44891
Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2023-02-18 12:56:21 +0800

    Decoupling stock movement conditions

--- app/Listeners/GenerateStockMovementFromValidatedTransferListener.php
+++ app/Listeners/GenerateStockMovementFromValidatedTransferListener.php
@@ -15,60 +15,74 @@ class GenerateStockMovementFromValidatedTransferListener implements ShouldQueue
     {
         $transfer = $event->transfer;
         $stockMovementData = [];
-        if ($transfer->isDone($transfer->status)) {
-            $transferLines = $transfer->transferLines;
-            foreach ($transferLines as $transferLine) {
-                $product = $transferLine->product;
-                $sourceLocationId = $transfer->source_location_id;
-                $destinationLocationId = $transfer->destination_location_id;
-                if (Product::isStorable($product->product_type)) {
-                    if ($transferLine->transferLineStockMovement()->exists()) {
-                        $stockMovement = $transferLine->transferLineStockMovement->stockMovement;
-                        $stockMovement->quantity_done = $transferLine->demand;
-                        $stockMovement = $stockMovement->toArray();
-                        unset($stockMovement['updated_at']);
-                        $stockMovementData[] = $stockMovement;
-                    } else {
-                        $stockMovementData[] = [
-                            'reference' => $transfer->reference,
-                            'source' => $transfer->reference,
-                            'product_id' => $transferLine->product_id,
-                            'source_location_id' => $sourceLocationId,
-                            'destination_location_id' => $destinationLocationId,
-                            'quantity_done' => $transferLine->demand,
-                            'created_at' => $transferLine->created_at,
-                        ];
-                    }
-                }
-                if ($product->material()->exists()) {
-                    ProductHasMaterial::dispatch(
-                        $transfer->reference,
-                        $transfer->reference,
-                        $sourceLocationId,
-                        $destinationLocationId,
-                        $product->material,
-                        $transferLine->demand
-                    );
-                }
-            }
+        if (!$transfer->isDone($transfer->status)) {
+            return;
         }
-        if (count($stockMovementData)) {
-            StockMovement::massUpsert($stockMovementData);
-            $transferLineStockMovementLines = [];
-            $transferLines = $transfer->transferLines;
-            foreach ($transferLines as $transferLine) {
-                $stockMovement = StockMovement::where('product_id', $transferLine->product_id)
-                    ->where('created_at', $transferLine->created_at)
-                    ->first();
-                $transferLineStockMovementLines[] = [
-                    'transfer_id' => $transfer->id,
-                    'transfer_line_id' => $transferLine->id,
-                    'stock_movement_id' => $stockMovement->id,
-                ];
+        $transferLines = $transfer->transferLines;
+        foreach ($transferLines as $transferLine) {
+            $stockMovement = $this->storableProductGenerateMovement($transfer, $transferLine);
+            if ($stockMovement) {
+                $stockMovementData[] = $stockMovement;
             }
-            if (count($transferLineStockMovementLines)) {
-                TransferLineStockMovement::massUpsert($transferLineStockMovementLines);
-            }
+            $this->hasMaterialGenerateMovement($transfer, $transferLine);
+        }
+        if (!count($stockMovementData)) {
+            return;
+        }
+        StockMovement::massUpsert($stockMovementData);
+        $transferLineStockMovementLines = [];
+        $transferLines = $transfer->transferLines;
+        foreach ($transferLines as $transferLine) {
+            $stockMovement = StockMovement::where('product_id', $transferLine->product_id)
+                ->where('created_at', $transferLine->created_at)
+                ->first();
+            $transferLineStockMovementLines[] = [
+                'transfer_id' => $transfer->id,
+                'transfer_line_id' => $transferLine->id,
+                'stock_movement_id' => $stockMovement->id,
+            ];
+        }
+        if (count($transferLineStockMovementLines)) {
+            TransferLineStockMovement::massUpsert($transferLineStockMovementLines);
+        }
+    }
+
+    private function storableProductGenerateMovement($transfer, $transferLine)
+    {
+        $product = $transferLine->product;
+        if (!Product::isStorable($product->product_type)) {
+            return false;
+        }
+        $stockMovement = [
+            'reference' => $transfer->reference,
+            'source' => $transfer->reference,
+            'product_id' => $transferLine->product_id,
+            'source_location_id' => $transfer->source_location_id,
+            'destination_location_id' => $transfer->destination_location_id,
+            'quantity_done' => $transferLine->demand,
+            'created_at' => $transferLine->created_at,
+        ];
+        if ($transferLine->transferLineStockMovement()->exists()) {
+            $stockMovement = $transferLine->transferLineStockMovement->stockMovement;
+            $stockMovement->quantity_done = $transferLine->demand;
+            $stockMovement = $stockMovement->toArray();
+            unset($stockMovement['updated_at']);
+        }
+        return $stockMovement;
+    }
+
+    private function hasMaterialGenerateMovement($transfer, $transferLine)
+    {
+        $product = $transferLine->product;
+        if ($product->material()->exists()) {
+            ProductHasMaterial::dispatch(
+                $transfer->reference,
+                $transfer->reference,
+                $transfer->source_location_id,
+                $transfer->destination_location_id,
+                $product->material,
+                $transferLine->demand
+            );
         }
     }
 }
