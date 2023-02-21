def29752c01f52cedbc5e28d323d8ce1775a7d99
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-02-21 14:41:01 +0800

    Manifesting global settings

--- resources/js/Modules/GlobalSetting/GlobalSettingManifest.js
+++ resources/js/Modules/GlobalSetting/GlobalSettingManifest.js
@@ -1,13 +1,11 @@
-import GlobalSettingForm from "./GlobalSettingForm";
-
 const displayName = "global_settings";
 
-export default {
+const manifest = {
     "moduleName": "global_settings",
     "displayName": displayName,
     "queryDefaults": {},
     "routes": [
-        {path: `/${displayName}`, component: GlobalSettingForm},
+        {path: `/${displayName}`, manifest: () => manifest},
     ],
     form: {
         initialValue: true,
@@ -207,160 +205,38 @@ export default {
                 row_1: {
                     col_1: [
                         {
-                            type: 'divider',
-                            name: 'divider_1',
-                            orientation: 'left',
-                            label: 'Measurements'
-                        },
-                        {
                             type: 'select',
-                            name: 'inventory_default_measurement_category_id',
-                            label: 'Default Measurement Category',
+                            name: 'accounting_default_currency_id',
+                            label: 'Default Currency',
                             query: {
-                                url: '/api/measurement_categories',
-                                field: 'inventory_default_measurement_category.name'
-                            },
-                        },
-                        {
-                            type: 'select',
-                            name: 'inventory_default_measurement_id',
-                            label: 'Default Measurement',
-                            query: {
-                                url: '/api/measurements',
-                                field: 'inventory_default_measurement.name'
-                            },
-                        },
-                        {
-                            type: 'select',
-                            name: 'inventory_default_purchase_measurement_id',
-                            label: 'Default Purchase Measurement',
-                            query: {
-                                url: '/api/measurements',
-                                field: 'inventory_default_purchase_measurement.name'
-                            },
-                        },
-                        {
-                            type: 'select',
-                            name: 'inventory_default_sales_measurement_id',
-                            label: 'Default Sales Measurement',
-                            query: {
-                                url: '/api/measurements',
-                                field: 'inventory_default_sales_measurement.name'
+                                url: '/api/currencies',
+                                field: 'accounting_default_currency.name'
                             },
                         },
                     ],
-                    col_2: [
-                        {
-                            type: 'divider',
-                            name: 'divider_1',
-                            orientation: 'left',
-                            label: 'Category'
-                        },
-                        {
-                            type: 'select',
-                            name: 'inventory_default_product_category_id',
-                            label: 'Default Product Category',
-                            query: {
-                                url: '/api/product_categories',
-                                field: 'inventory_default_product_category.name'
-                            },
-                        },
-                    ]
                 },
-                divider_1: {
-                    type: 'divider',
-                    name: 'divider_1',
-                    orientation: 'left',
-                    label: 'Locations'
-                },
-                row_2: {
+            },
+            tab_pane_4: {
+                name: 'Sales',
+                row_1: {
                     col_1: [
                         {
                             type: 'select',
-                            name: 'inventory_default_customer_location_id',
-                            label: 'Default Customer Location',
+                            name: 'sales_order_default_sequence_id',
+                            label: 'Sales Default Sequence',
                             query: {
-                                url: '/api/locations',
-                                field: 'inventory_default_customer_location.name'
+                                url: '/api/sequences',
+                                field: 'sales_order_default_sequence.name'
                             },
                         },
                         {
                             type: 'select',
-                            name: 'inventory_default_vendor_location_id',
-                            label: 'Default Vendor Location',
+                            name: 'sales_order_default_delivery_fee_id',
+                            label: 'Sales Default Delivery Fee',
                             query: {
-                                url: '/api/locations',
-                                field: 'inventory_default_vendor_location.name'
+                                url: '/api/delivery_fees',
+                                field: 'sales_order_default_delivery_fee.name'
                             },
-                        },
-                        {
-                            type: 'select',
-                            name: 'inventory_default_adjustment_location_id',
-                            label: 'Default Adjustment Location',
-                            query: {
-                                url: '/api/locations',
-                                field: 'inventory_default_adjustment_location.name'
-                            },
-                        },
-                    ],
-                    col_2: [
-                        {
-                            type: 'select',
-                            name: 'inventory_default_production_id',
-                            label: 'Default Production Location',
-                            query: {
-                                url: '/api/locations',
-                                field: 'inventory_default_production.name'
-                            },
-                        },
-                        {
-                            type: 'select',
-                            name: 'inventory_default_scrap_id',
-                            label: 'Default Scrap Location',
-                            query: {
-                                url: '/api/locations',
-                                field: 'inventory_default_scrap.name'
-                            },
-                        },
-                    ]
-                },
-                divider_2: {
-                    type: 'divider',
-                    name: 'divider_2',
-                    orientation: 'left',
-                    label: 'Defaults'
-                },
-                row_3: {
-                    col_1: [
-                        {
-                            type: 'select',
-                            name: 'inventory_default_warehouse_id',
-                            label: 'Default Warehouse',
-                            query: {
-                                url: '/api/warehouses',
-                                field: 'inventory_default_warehouse.name'
-                            },
-                        },
-
-                    ],
-                },
-                divider_3: {
-                    type: 'divider',
-                    name: 'divider_3',
-                    orientation: 'left',
-                    label: 'Background'
-                },
-                row_4: {
-                    col_1: [
-                        {
-                            type: 'checkbox',
-                            name: 'inventory_auto_validate_draft',
-                            label: 'Auto validate drafts',
-                        },
-                        {
-                            type: 'checkbox',
-                            name: 'inventory_compute_product_quantity',
-                            label: 'Compute Product Quantity',
                         },
                     ],
                 },
@@ -368,3 +244,5 @@ export default {
         }
     }
 };
+
+export default manifest;
