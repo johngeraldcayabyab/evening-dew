394d216460f96b76bb5d7c0659a4192fe4006a55
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-02-24 09:28:17 +0800

    Make resource readable

--- app/Traits/ResourceHelper.php
+++ app/Traits/ResourceHelper.php
@@ -10,32 +10,33 @@ trait ResourceHelper
     public function defaults($object, $request, $resource)
     {
         $timeStampFormat = 'm/d/Y h:i:s A';
-        $selectedResource = $this->getSelectedFieldsOnly($request, $resource);
-        $selectedResource['id'] = $object->id;
+        $filteredResource = $this->filterResource($request, $resource);
+        $filteredResource['id'] = $object->id;
         if ($object->created_at) {
-            $selectedResource['created_at'] = $object->created_at->format($timeStampFormat);
+            $filteredResource['created_at'] = $object->created_at->format($timeStampFormat);
         }
         if ($object->updated_at) {
-            $selectedResource['updated_at'] = $object->updated_at->format($timeStampFormat);
+            $filteredResource['updated_at'] = $object->updated_at->format($timeStampFormat);
         }
         if ($object->deleted_at) {
-            $selectedResource['deleted_at'] = $object->deleted_at->format($timeStampFormat);
+            $filteredResource['deleted_at'] = $object->deleted_at->format($timeStampFormat);
         }
         if ($object->id) {
-            $selectedResource['next_record'] = $object->nextRecord($object->id);
-            $selectedResource['previous_record'] = $object->previousRecord($object->id);
+            $filteredResource['next_record'] = $object->nextRecord($object->id);
+            $filteredResource['previous_record'] = $object->previousRecord($object->id);
         }
-        return $selectedResource;
+        return $filteredResource;
     }
 
-    private function getSelectedFieldsOnly($request, $resource)
+    private function filterResource($request, $resource)
     {
         $selectedResourceFields = $resource;
-        if ($request->selected_fields) {
-            if ($this->isRequestForTable($request)) {
-                $selectedFields = explode(',', $request->selected_fields);
-                $selectedResourceFields = Arr::only($selectedResourceFields, $selectedFields);
-            }
+        if (!$request->selected_fields) {
+            return $selectedResourceFields;
+        }
+        if ($this->isRequestForTable($request)) {
+            $selectedFields = explode(',', $request->selected_fields);
+            $selectedResourceFields = Arr::only($selectedResourceFields, $selectedFields);
         }
         return $selectedResourceFields;
     }
