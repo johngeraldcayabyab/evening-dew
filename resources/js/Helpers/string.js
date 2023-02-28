103ca08c67c7c2b3921f6353b00e793540df4595
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-02-28 13:32:38 +0800

    Added float parser for comma

--- resources/js/Helpers/string.js
+++ resources/js/Helpers/string.js
@@ -29,6 +29,10 @@ export const numberWithCommas = (x) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
+export const parseFloatComma = (float) => {
+    return parseFloat(float.replace(',', ''));
+}
+
 export const listKey = (name) => {
     return `${name}-${LIST}`;
 };
