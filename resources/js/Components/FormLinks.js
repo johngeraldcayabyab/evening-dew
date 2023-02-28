5216e4576a262f244366771bd5a9d601c9940a0c
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-02-28 16:24:09 +0800

    Refactoring form links

--- resources/js/Components/FormLinks.js
+++ resources/js/Components/FormLinks.js
@@ -9,68 +9,69 @@ import {useContext} from "react";
 
 const FormLinks = (props) => {
     const formContext = useContext(FormContext);
+    const initialValues = formContext.formState.initialValues;
 
     let links = [];
 
     function labelizer(label) {
-        if(typeof label === 'function'){
-            return label(formContext.formState.initialValues);
+        if (typeof label === 'function') {
+            return label(initialValues);
         }
         return label;
     }
 
-    if (objectHasValue(formContext.formState.initialValues)) {
+    if (objectHasValue(initialValues)) {
         props.links.forEach((link) => {
             /**
              * this needs to be refactored
              */
-            if (objectHasValue(formContext.formState.initialValues[link.value])) {
+            if (objectHasValue(initialValues[link.value])) {
                 links.push(link);
             }
         });
-        if (links.length) {
-            return (
-                <div style={{
-                    borderBottom: '1px solid #cccccc',
-                    paddingBottom: '11px',
-                    marginBottom: '10px'
-                }}>
-                    <RowForm align={'right'}>
-                        <ColForm lg={24} style={{textAlign: 'right'}}>
-                            {links.map(link => {
-                                if (link.module) {
-                                    return (
-                                        <Link
-                                            key={uuidv4()}
-                                            to={`/${link.module}?${link.param}=${formContext.formState.initialValues[link.value]}`}>
-                                            <Button
-                                                htmlType={"button"}
-                                                type={"ghost"}
-                                                size={'default'}
-                                            >
-                                                {labelizer(link.label)}
-                                            </Button>
-                                        </Link>
-                                    )
-                                }
-                                return (
+        if (!links.length) {
+            return null;
+        }
+    }
+    return (
+        <div style={{
+            borderBottom: '1px solid #cccccc',
+            paddingBottom: '11px',
+            marginBottom: '10px'
+        }}>
+            <RowForm align={'right'}>
+                <ColForm lg={24} style={{textAlign: 'right'}}>
+                    {links.map(link => {
+                        if (link.module) {
+                            return (
+                                <Link
+                                    key={uuidv4()}
+                                    to={`/${link.module}?${link.param}=${initialValues[link.value]}`}>
                                     <Button
-                                        key={uuidv4()}
                                         htmlType={"button"}
                                         type={"ghost"}
                                         size={'default'}
                                     >
                                         {labelizer(link.label)}
                                     </Button>
-                                )
-                            })}
-                        </ColForm>
-                    </RowForm>
-                </div>
-            )
-        }
-    }
-    return null;
+                                </Link>
+                            )
+                        }
+                        return (
+                            <Button
+                                key={uuidv4()}
+                                htmlType={"button"}
+                                type={"ghost"}
+                                size={'default'}
+                            >
+                                {labelizer(link.label)}
+                            </Button>
+                        )
+                    })}
+                </ColForm>
+            </RowForm>
+        </div>
+    );
 };
 
 export default FormLinks;
