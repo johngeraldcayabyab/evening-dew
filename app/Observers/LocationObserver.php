674cd55d12bdd5fd9cfc6e77e53d12234f0fee9d
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-02-28 16:43:01 +0800

    Added parent location checker, should only apply to internal types

--- app/Observers/LocationObserver.php
+++ app/Observers/LocationObserver.php
@@ -9,15 +9,28 @@ class LocationObserver
 {
     public function creating(Location $location)
     {
+        $this->checkInternalTypes($location);
         $this->setDefaults($location);
     }
 
     public function updating(Location $location)
     {
+        $this->checkInternalTypes($location);
         if ($location->parent_location_id === $location->id) {
             throw ValidationException::withMessages(['parent_location_id' => 'Recursion detected']);
         }
         $this->setDefaults($location);
+    }
+
+    public function checkInternalTypes(Location $location)
+    {
+        if ($location->type === Location::INTERNAL) {
+            if ($location->parentLocation()->exists()) {
+                if ($location->parentLocation->type !== Location::VIEW) {
+                    throw ValidationException::withMessages(['parent_location_id' => 'Parent location for internal types should be View types']);
+                }
+            }
+        }
     }
 
     public function setDefaults($model)
