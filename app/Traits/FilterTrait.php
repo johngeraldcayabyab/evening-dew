2562d913dd282a2f58c8106b5f82709e08f5c251
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-03-03 11:22:01 +0800

    N+1 Problem solved for filter

--- app/Traits/FilterTrait.php
+++ app/Traits/FilterTrait.php
@@ -20,6 +20,16 @@ trait FilterTrait
         $query = $this;
         $modelInstance = $this;
         $fields = $query->getFields();
+        $relationships = [];
+        foreach ($fields as $field) {
+            $relationship = $this->hasRelationGet($modelInstance, $field);
+            if ($relationship) {
+                $relationships[] = $relationship;
+            }
+        }
+        if (count($relationships)) {
+            $query = $query->with($relationships);
+        }
         $query = $this->groupNow($request, $query);
         $query = $this->filterNowGlobal($fields, $request, $modelInstance, $query);
         $query = $this->filterNow($fields, $request, $modelInstance, $query);
