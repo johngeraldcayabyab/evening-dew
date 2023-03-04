3ef8153f0331a41c5bf2f785e98e2043bcd79439
Gerald Cayabyab <johngeraldcayabyab@gmail.com>
2023-03-05 02:33:00 +0800

    Query now closer to ORM with pagination

--- app/Http/Controllers/StockLocationQuantityController.php
+++ app/Http/Controllers/StockLocationQuantityController.php
@@ -17,39 +17,15 @@ class StockLocationQuantityController extends Controller
 
     public function index(Request $request)
     {
-        /**
-         * select
-         *      COUNT(stock.product_id),
-         *      COUNT(stock.destination_location_id),
-         *      prod.name,
-         *      loc.name,
-         *      ((select COALESCE(sum(stock_destination.quantity_done), 0)
-         * from stock_movements as stock_destination
-         *  where stock_destination.product_id = prod.id
-         *  and stock_destination.destination_location_id = loc.id) -
-         *  hnu865u(select COALESCE(sum(stock_source.quantity_done), 0)
-         *  from stock_movements as stock_source
-         *  where stock_source.product_id = prod.id
-         *  and stock_source.source_location_id = loc.id)) as quantity
-         *  from stock_movements as stock
-         *  left join products as prod on prod.id = stock.product_id
-         *  left join locations as loc on loc.id = stock.destination_location_id
-         * where stock.destination_location_id in (select id from locations where type = 'internal')
-         * group by stock.product_id, stock.destination_location_id;
-         */
-        return [];
-//        return Location::where('type', Location::INTERNAL)
-//            ->get()
-//            ->map(function ($internalLocation) {
-//                $stockMovementSourceLocationSum = $internalLocation->stockMovementSources()
-//                    ->where('source_location_id', $internalLocation->id)
-//                    ->pluck('quantity_done')
-//                    ->sum();
-//                $stockMovementDestinationLocationSum = $internalLocation->stockMovementDestinations()
-//                    ->where('destination_location_id', $internalLocation->id)
-//                    ->pluck('quantity_done')
-//                    ->sum();
-//                return $stockMovementDestinationLocationSum - $stockMovementSourceLocationSum;
-//            });
+        $internalLocationIds = Location::where('type', Location::INTERNAL)->pluck('id');
+        $destinationRaw = 'select COALESCE(sum(stock_destination.quantity_done), 0) from stock_movements as stock_destination where stock_destination.product_id = products.id and stock_destination.destination_location_id = locations.id';
+        $sourceRaw = 'select COALESCE(sum(stock_source.quantity_done), 0) from stock_movements as stock_source where stock_source.product_id = products.id and stock_source.source_location_id = locations.id';
+        $rawQuery = "(($destinationRaw) - ($sourceRaw)) as quantity";
+        return StockMovement::selectRaw("COUNT(stock_movements.product_id) as product_aggregate, COUNT(stock_movements.destination_location_id) as location_aggregate, products.name as product_name, locations.name as location_name, $rawQuery")
+            ->leftJoin('products', 'products.id', '=', 'stock_movements.product_id')
+            ->leftJoin('locations', 'locations.id', '=', 'stock_movements.destination_location_id')
+            ->whereIn('destination_location_id', $internalLocationIds)
+            ->groupBy('stock_movements.product_id', 'stock_movements.destination_location_id')
+            ->paginate();
     }
 }
