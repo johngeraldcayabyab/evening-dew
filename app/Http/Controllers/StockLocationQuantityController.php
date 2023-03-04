345fffe8d8e04e1bb86a330ad1d223a5f9536f1e
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-03-04 08:13:25 +0800

    Save query as comment for now

--- app/Http/Controllers/StockLocationQuantityController.php
+++ app/Http/Controllers/StockLocationQuantityController.php
@@ -17,18 +17,39 @@ class StockLocationQuantityController extends Controller
 
     public function index(Request $request)
     {
-        return Location::where('type', Location::INTERNAL)
-            ->get()
-            ->map(function ($internalLocation) {
-                $stockMovementSourceLocationSum = $internalLocation->stockMovementSources()
-                    ->where('source_location_id', $internalLocation->id)
-                    ->pluck('quantity_done')
-                    ->sum();
-                $stockMovementDestinationLocationSum = $internalLocation->stockMovementDestinations()
-                    ->where('destination_location_id', $internalLocation->id)
-                    ->pluck('quantity_done')
-                    ->sum();
-                return $stockMovementDestinationLocationSum - $stockMovementSourceLocationSum;
-            });
+        /**
+         * select
+         *      COUNT(stock.product_id),
+         *      COUNT(stock.destination_location_id),
+         *      prod.name,
+         *      loc.name,
+         *      ((select COALESCE(sum(stock_destination.quantity_done), 0)
+         * from stock_movements as stock_destination
+         *  where stock_destination.product_id = prod.id
+         *  and stock_destination.destination_location_id = loc.id) -
+         *  hnu865u(select COALESCE(sum(stock_source.quantity_done), 0)
+         *  from stock_movements as stock_source
+         *  where stock_source.product_id = prod.id
+         *  and stock_source.source_location_id = loc.id)) as quantity
+         *  from stock_movements as stock
+         *  left join products as prod on prod.id = stock.product_id
+         *  left join locations as loc on loc.id = stock.destination_location_id
+         * where stock.destination_location_id in (select id from locations where type = 'internal')
+         * group by stock.product_id, stock.destination_location_id;
+         */
+        return [];
+//        return Location::where('type', Location::INTERNAL)
+//            ->get()
+//            ->map(function ($internalLocation) {
+//                $stockMovementSourceLocationSum = $internalLocation->stockMovementSources()
+//                    ->where('source_location_id', $internalLocation->id)
+//                    ->pluck('quantity_done')
+//                    ->sum();
+//                $stockMovementDestinationLocationSum = $internalLocation->stockMovementDestinations()
+//                    ->where('destination_location_id', $internalLocation->id)
+//                    ->pluck('quantity_done')
+//                    ->sum();
+//                return $stockMovementDestinationLocationSum - $stockMovementSourceLocationSum;
+//            });
     }
 }
