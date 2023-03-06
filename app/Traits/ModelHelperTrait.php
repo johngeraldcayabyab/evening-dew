b249c77d7b5a6bec8537415e5072300f4270bbd2
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-03-06 13:15:16 +0800

    Added queryable raw sql scope

--- app/Traits/ModelHelperTrait.php
+++ app/Traits/ModelHelperTrait.php
@@ -8,4 +8,12 @@ trait ModelHelperTrait
     {
         return $query->whereIn('id', $ids)->delete();
     }
+
+    public function scopeSqlRaw($query)
+    {
+        $raw = str_replace(array('?'), array('\'%s\''), $query->toSql());
+        $raw = vsprintf($raw, $query->getBindings());
+        $raw = str_replace("'", '', $raw);
+        return str_replace("`", '', $raw);
+    }
 }
