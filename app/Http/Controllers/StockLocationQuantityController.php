7983408af6161c1bcc94a2c4be1b0b735eb2290d
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-03-06 14:56:24 +0800

    Stock location quantity query now in eloquent(ORM) format

--- app/Http/Controllers/StockLocationQuantityController.php
+++ app/Http/Controllers/StockLocationQuantityController.php
@@ -16,12 +16,27 @@ class StockLocationQuantityController extends Controller
     public function index(Request $request): ResourceCollection
     {
         $internalLocationIds = Location::where('type', Location::INTERNAL)->pluck('id');
-        $destinationRaw = 'select COALESCE(sum(stock_destination.quantity_done), 0) from stock_movements as stock_destination where stock_destination.product_id = products.id and stock_destination.destination_location_id = locations.id';
-        $sourceRaw = 'select COALESCE(sum(stock_source.quantity_done), 0) from stock_movements as stock_source where stock_source.product_id = products.id and stock_source.source_location_id = locations.id';
+        $destinationRaw = StockMovement::from('stock_movements as stock_destination')
+            ->selectRaw('COALESCE(sum(stock_destination.quantity_done), 0)')
+            ->where('stock_destination.product_id', 'products.id')
+            ->where('stock_destination.destination_location_id', 'locations.id')
+            ->sqlRaw();
+        $sourceRaw = StockMovement::from('stock_movements as stock_source')
+            ->selectRaw('COALESCE(sum(stock_source.quantity_done), 0)')
+            ->where('stock_source.product_id', 'products.id')
+            ->where('stock_source.source_location_id', 'locations.id')
+            ->sqlRaw();
         $rawQuery = "(($destinationRaw) - ($sourceRaw)) as quantity";
-        $stockLocationQuantity = StockMovement::selectRaw("COUNT(stock_movements.product_id) as product_aggregate, COUNT(stock_movements.destination_location_id) as location_aggregate, products.name as product_name, locations.name as location_name, $rawQuery")
-            ->leftJoin('products', 'products.id', '=', 'stock_movements.product_id')
-            ->leftJoin('locations', 'locations.id', '=', 'stock_movements.destination_location_id')
+        $stockLocationQuantity = StockMovement::from('stock_movements as stock_movement_quantity')
+            ->selectRaw("
+                COUNT(stock_movement_quantity.product_id) as product_aggregate,
+                COUNT(stock_movement_quantity.destination_location_id) as location_aggregate,
+                products.name as product_name,
+                locations.name as location_name,
+                $rawQuery"
+            )
+            ->leftJoin('products', 'products.id', '=', 'stock_movement_quantity.product_id')
+            ->leftJoin('locations', 'locations.id', '=', 'stock_movement_quantity.destination_location_id')
             ->whereIn('destination_location_id', $internalLocationIds)
             ->groupBy('stock_movements.product_id', 'stock_movements.destination_location_id')
             ->paginate();
