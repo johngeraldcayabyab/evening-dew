0495e004a59a47f1ec8d0610b9ee35ecb8642451
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-03-21 12:42:05 +0800

    Line column readability improvement

--- resources/js/Components/FormLines/LineColumn.js
+++ resources/js/Components/FormLines/LineColumn.js
@@ -1,16 +1,23 @@
 import FormLabel from "../Typography/FormLabel";
 import RowForm from "../Grid/RowForm";
 import ColForm from "../Grid/ColForm";
-import React, {useContext} from "react";
-import {FormContext} from "../../Contexts/FormContext";
+import React from "react";
 
 const LineColumn = (props) => {
-    const formContext = useContext(FormContext);
-
     const columnLength = props.columns.length;
     const width = 100 / columnLength;
     const columns = props.columns.map((column) => {
-        return (<FormLabel key={column} style={{display: 'inline-block', width: `${width}%`}}>{column}</FormLabel>);
+        return (
+            <FormLabel
+                key={column}
+                style={{
+                    display: 'inline-block',
+                    width: `${width}%`
+                }}
+            >
+                {column}
+            </FormLabel>
+        );
     });
     return (
         <RowForm>
