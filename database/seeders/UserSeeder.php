34d806b166e642cdd7d2081ce2a1beb7b2ee6008
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-03-29 23:23:40 +0800

    Added a system user for logging and preparation for php8.2 migration

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,11 +6,18 @@ use App\Events\UserCreated;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
     public function run()
     {
+        User::create([
+            'name' => 'System',
+            'email' => 'system@system.system',
+            'password' => Hash::make(Str::uuid()),
+            'app_menu_id' => 1
+        ]);
         $admin = User::create([
             'name' => 'Admin',
             'email' => 'admin@example.com',
