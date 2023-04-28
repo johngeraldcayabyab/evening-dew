60eb5d62e01218d2eed94187126cf32c6fdbae21
Gerald <johngeraldcayabyab@gmail.com>
2023-04-29 01:53:31 +0800

    fix dropdown button

--- resources/js/Components/TableAndKanban/TableButtons/ActionsDropdownButton.jsx
+++ resources/js/Components/TableAndKanban/TableButtons/ActionsDropdownButton.jsx
@@ -1,55 +1,53 @@
-import {Dropdown, Menu, Popconfirm, Space} from "antd";
-import ListDeleteButton from "./ListDeleteButton";
+import {Button, Dropdown, Popconfirm} from "antd";
 import {TableContext} from "../../../Contexts/TableContext";
 import React, {useContext} from "react";
-import ListExportButton from "./ListExportButton";
+import {DownOutlined} from "@ant-design/icons";
+import {toQueryString} from "../../../Helpers/url";
+import {Link} from "react-router-dom";
 
 const ActionsDropdownButton = () => {
     const tableContext = useContext(TableContext);
-
-    return(
-        <Dropdown
-            menu={{
-                ,
+    const params = toQueryString(tableContext.state.params);
+    const exportLink = tableContext.exportLink ? `${tableContext.exportLink}/?${params}` : `${tableContext.manifest.moduleName}/export?${params}`;
+    const items = [
+        {
+            key: 'delete',
+            label: <Popconfirm
+                title={`Are you sure you want to delete the selected items?`}
+                okText="Yes"
+                cancelText="No" onConfirm={() => {
+                let ids = tableContext.state.selectedRows.map((row) => (row.id));
+                tableContext.handleMassDelete(ids);
             }}
-        >
-            <a onClick={(e) => e.preventDefault()}>
-                <Space>
-                    Hover me
-                    <DownOutlined />
-                </Space>
-            </a>
-        </Dropdown>
-    )
+            >
+                Delete
+            </Popconfirm>
+        },
+        {
+            key: 'export',
+            label: <Popconfirm
+                title={`Are you sure you want to export these items?`}
+                okText={
+                    <Link to={exportLink} target="_blank" rel="noopener noreferrer">
+                        Yes
+                    </Link>
+                }
+                cancelText="No"
+            >
+                Export
+            </Popconfirm>
+        }
+    ];
 
-    // const menu = (
-    //     <Menu>
-    //         <ListDeleteButton/>
-    //         <ListExportButton/>
-    //     </Menu>
-    // );
-
-    // if (tableContext.state.selectedRows.length) {
-    //     return (
-    //         <Dropdown.Button menu={[
-    //             {
-    //                 key: 'delete',
-    //                 label: <Popconfirm
-    //                     title={`Are you sure you want to delete the selected items?`}
-    //                     okText="Yes"
-    //                     cancelText="No" onConfirm={() => {
-    //                     let ids = tableContext.state.selectedRows.map((row) => (row.id));
-    //                     tableContext.handleMassDelete(ids);
-    //                 }}
-    //                 >
-    //                     Delete
-    //                 </Popconfirm>
-    //             }
-    //         ]}>Actions</Dropdown.Button>
-    //     )
-    // } else {
-    //     return null;
-    // }
+    if (tableContext.state.selectedRows.length) {
+        return (
+            <Dropdown menu={{items}}>
+                <Button>Actions <DownOutlined/></Button>
+            </Dropdown>
+        )
+    } else {
+        return null;
+    }
 }
 
 export default ActionsDropdownButton;
