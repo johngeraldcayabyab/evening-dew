40373a95a989351accc9c441acb3a0af34326c5f
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-05-03 15:19:31 +0800

    fix incorrect product category id spelling

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -22,7 +22,7 @@ class ProductController
         $model = $model->filterAndOrder($request, function ($query) use ($request) {
             if (Str::contains($request->selected_fields, 'slug') && !$request->id) {
                 $query = $query->where('product_category_id', '!=', 5);
-                $query = $query->where('prodcut_category_id', '!=', 6);
+                $query = $query->where('product_category_id', '!=', 6);
             }
             return $query;
         });
