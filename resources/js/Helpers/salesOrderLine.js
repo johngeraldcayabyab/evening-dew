876802e0a7d5958d129a3d36e3019d3f6c687b95
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-10-06 17:49:52 +0800

    Sales Order line computation bug fix on subtotal NaN

--- resources/js/Helpers/salesOrderLine.js
+++ resources/js/Helpers/salesOrderLine.js
@@ -4,10 +4,12 @@ export const computeSalesOrderLineSubtotal = (salesOrderLine, taxes) => {
     if (salesOrderLine) {
         salesOrderLine.taxable_amount = 0;
         salesOrderLine.tax_amount = 0;
-        salesOrderLine.subtotal = salesOrderLine.quantity * salesOrderLine.unit_price;
-        if (salesOrderLine.tax_id) {
-            const tax = getTax(salesOrderLine.tax_id, taxes);
-            salesOrderLine = computeTax(tax, salesOrderLine);
+        if (salesOrderLine.quantity && salesOrderLine.unit_price) {
+            salesOrderLine.subtotal = salesOrderLine.quantity * salesOrderLine.unit_price;
+            if (salesOrderLine.tax_id) {
+                const tax = getTax(salesOrderLine.tax_id, taxes);
+                salesOrderLine = computeTax(tax, salesOrderLine);
+            }
         }
     }
     return salesOrderLine;
