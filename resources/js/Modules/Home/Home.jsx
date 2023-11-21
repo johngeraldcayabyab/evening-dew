f741d8ef6b5dd8ba2f15656b51819b4d73671996
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2023-11-21 12:11:43 +0800

    change homepage

--- resources/js/Modules/Home/Home.jsx
+++ resources/js/Modules/Home/Home.jsx
@@ -1,5 +1,18 @@
+import {Card} from "antd"
+
 const Home = () => {
-    return 'Homey';
+    return (
+        <Card
+            title="Dashboard"
+            bordered={false}
+            style={{
+                width: 300,
+                margin: 20
+            }}
+        >
+            <p>You are now logged in!</p>
+        </Card>
+    );
 };
 
 export default Home;
