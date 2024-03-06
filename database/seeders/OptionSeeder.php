d442d8fa4ef9a094d93ae0c9f9b995991e86289e
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2024-03-07 04:32:39 +0800

    option seeder added transfer pdf column views

--- database/seeders/OptionSeeder.php
+++ database/seeders/OptionSeeder.php
@@ -10,6 +10,10 @@ class OptionSeeder extends Seeder
     public function run(): void
     {
         Option::create([
+            'name' => 'transfer_lines_pdf_columns_view',
+            'value' => 'product,description,demand,avatar'
+        ]);
+        Option::create([
             'name' => 'sales_order_lines_pdf_columns_view',
             'value' => 'product,description,quantity,unit_price,tax,subtotal,avatar'
         ]);
