b911fe0e45436b99b5fdcf40a46513b6165f8874
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2024-09-03 17:13:47 +0800

    added more seeder data

--- database/seeders/OptionSeeder.php
+++ database/seeders/OptionSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Option;
-use App\Models\SalesOrderLine;
 use Illuminate\Database\Seeder;
 
 class OptionSeeder extends Seeder
@@ -22,13 +21,44 @@ class OptionSeeder extends Seeder
             'name' => 'sales_order_breakdown_view',
             'value' => 'taxable_amount,tax_amount,discount,total'
         ]);
+        /**
+         * By default, everything starts in subtotal
+         */
         Option::create([
-            'name' => 'sales_order_lines_discount_active',
-            'value' => '0'
+            'name' => 'sales_order_computation_order',
+            'value' => 'discount|tax' // if you want to change the order, put the tax first tax|discount
         ]);
         Option::create([
-            'name' => 'sales_order_lines_discount_type_default',
-            'value' => SalesOrderLine::PERCENTAGE
+            'name' => 'sales_order_tax_computation_order',
+            'value' => 'subtotal' // subtotal | per_unit_price
+        ]);
+        Option::create([
+            'name' => 'sales_order_discount_computation_order',
+            'value' => 'subtotal' // subtotal | per_unit_price
+        ]);
+        Option::create([
+            'name' => 'purchase_computation_order',
+            'value' => 'discount|tax' // if you want to change the order, put the tax first tax|discount
+        ]);
+        Option::create([
+            'name' => 'purchase_tax_computation_order',
+            'value' => 'subtotal' // subtotal | per_unit_price
+        ]);
+        Option::create([
+            'name' => 'purchase_discount_computation_order',
+            'value' => 'subtotal' // subtotal | per_unit_price
+        ]);
+        Option::create([
+            'name' => 'invoice_computation_order',
+            'value' => 'discount|tax' // if you want to change the order, put the tax first tax|discount
+        ]);
+        Option::create([
+            'name' => 'invoice_tax_computation_order',
+            'value' => 'subtotal' // subtotal | per_unit_price
+        ]);
+        Option::create([
+            'name' => 'invoice_discount_computation_order',
+            'value' => 'subtotal' // subtotal | per_unit_price
         ]);
     }
 }
