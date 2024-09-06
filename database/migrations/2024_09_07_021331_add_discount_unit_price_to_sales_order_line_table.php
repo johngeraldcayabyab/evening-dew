ee837e773ee4e1cae43e669ed2246558a26ea579
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2024-09-07 02:14:40 +0800

    added discounted unit price in sales order lines table

---
+++ database/migrations/2024_09_07_021331_add_discount_unit_price_to_sales_order_line_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('sales_order_lines', function (Blueprint $table) {
+            $table->double('discount_unit_price')->nullable()->default(0)->after('unit_price');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('sales_order_lines', function (Blueprint $table) {
+            $table->dropColumn('discount_unit_price');
+        });
+    }
+};
