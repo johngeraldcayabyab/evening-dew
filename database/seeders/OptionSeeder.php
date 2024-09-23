fdda297c10ee7ab25d7e071e08ea1a4ed3cae79d
John Gerald B. Cayabyab <johngeraldcayabyab@gmail.com>
2024-09-23 17:15:52 +0800

    remove order

--- database/seeders/OptionSeeder.php
+++ database/seeders/OptionSeeder.php
@@ -21,44 +21,5 @@ class OptionSeeder extends Seeder
             'name' => 'sales_order_breakdown_view',
             'value' => 'taxable_amount,tax_amount,discount,total'
         ]);
-        /**
-         * By default, everything starts in subtotal
-         */
-        Option::create([
-            'name' => 'sales_order_computation_order',
-            'value' => 'discount' // if you want to change the order, put the tax first tax|discount
-        ]);
-        Option::create([
-            'name' => 'sales_order_tax_computation_order',
-            'value' => 'subtotal' // subtotal | per_unit_price
-        ]);
-        Option::create([
-            'name' => 'sales_order_discount_computation_order',
-            'value' => 'subtotal' // subtotal | per_unit_price
-        ]);
-        Option::create([
-            'name' => 'purchase_computation_order',
-            'value' => 'discount' // if you want to change the order, put the tax first tax|discount
-        ]);
-        Option::create([
-            'name' => 'purchase_tax_computation_order',
-            'value' => 'subtotal' // subtotal | per_unit_price
-        ]);
-        Option::create([
-            'name' => 'purchase_discount_computation_order',
-            'value' => 'subtotal' // subtotal | per_unit_price
-        ]);
-        Option::create([
-            'name' => 'invoice_computation_order',
-            'value' => 'discount' // if you want to change the order, put the tax first tax|discount
-        ]);
-        Option::create([
-            'name' => 'invoice_tax_computation_order',
-            'value' => 'subtotal' // subtotal | per_unit_price
-        ]);
-        Option::create([
-            'name' => 'invoice_discount_computation_order',
-            'value' => 'subtotal' // subtotal | per_unit_price
-        ]);
     }
 }
